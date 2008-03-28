940750489f0accf15789dca2860f62bd8fdf835c
Vegard Bønes <vegard.bones@met.no>
2008-03-28 14:07:23 +0000

    installation now sets up cache database for kvAgregated
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@276 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvAgregated/agregateDbInit.sh
+++ src/kvAgregated/agregateDbInit.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
-cd $KVALOBS/var
+VARDIR=$1
+if [ ! -d $VARDIR ]; then
+	mkdir $VARDIR
+fi  
 
-if [ ! $? ]; then
-    echo "Cant change directory to $KVALOBS/var!"
-    echo "Solve the problem and try again!"
-    exit 1
+mkdir -p $VARDIR/agregate
+DATABASE=$VARDIR/agregate/database.sqlite
+if [ -f $DATABASE ]; then
+	rm $DATABASE
 fi
 
-
-pwd
-mkdir -p agregate
-cd agregate
-
-if [ ! $? ]; then
-    echo "Cant change directory to $KVALOBS/var/agregate!"
-    echo "Solve the problem and try again!"
-    exit 1
-fi
-
-pwd
-
-sqlite database.sqlite "CREATE TABLE data (stationid INTEGER NOT NULL,obstime TIMESTAMP NOT NULL,original FLOAT NOT NULL,paramid INTEGER NOT NULL,tbtime TIMESTAMP NOT NULL,typeid INTEGER NOT NULL,sensor CHAR(1) DEFAULT '0',level INTEGER DEFAULT 0,corrected FLOAT NOT NULL,controlinfo CHAR(16) DEFAULT '0000000000000000',useinfo CHAR(16) DEFAULT '0000000000000000',cfailed TEXT DEFAULT NULL,UNIQUE ( stationid, obstime, paramid, level, sensor, typeid ));"
+sqlite $DATABASE "CREATE TABLE data (stationid INTEGER NOT NULL,obstime TIMESTAMP NOT NULL,original FLOAT NOT NULL,paramid INTEGER NOT NULL,tbtime TIMESTAMP NOT NULL,typeid INTEGER NOT NULL,sensor CHAR(1) DEFAULT '0',level INTEGER DEFAULT 0,corrected FLOAT NOT NULL,controlinfo CHAR(16) DEFAULT '0000000000000000',useinfo CHAR(16) DEFAULT '0000000000000000',cfailed TEXT DEFAULT NULL,UNIQUE ( stationid, obstime, paramid, level, sensor, typeid ));"
