36e05d97f3f83cf47fe8bd0105022fef347ff80c
Børge Moe <borge.moe@met.no>
2008-04-11 13:27:55 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@338 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvsynopd/createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE data (stationid integer, obstime timestamp, original text, paramid integer, typeid integer, sensor character(1), level integer, UNIQUE(stationid, obstime, paramid, typeid, level, sensor));
-
-CREATE TABLE synop(wmono integer, obstime timestamp, createtime timestamp, crc integer, ccx integer, wmomsg text, UNIQUE(wmono, obstime));
-
-CREATE TABLE waiting(wmono integer, obstime timestamp, delaytime timestamp, UNIQUE(wmono, obstime));
-
-CREATE TABLE keyval(key text, val text, UNIQUE(key));
-
