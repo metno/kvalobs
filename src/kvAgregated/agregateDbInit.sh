1f222c51da9c7ac30a8fe7b6135cff221c09888d
Børge Moe <borge.moe@met.no>
2008-06-05 12:38:11 +0000

    Changed to use sqlite3.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@422 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvAgregated/agregateDbInit.sh
+++ src/kvAgregated/agregateDbInit.sh
@@ -27,4 +27,4 @@
 #  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
-sqlite $1 "CREATE TABLE data (stationid INTEGER NOT NULL,obstime TIMESTAMP NOT NULL,original FLOAT NOT NULL,paramid INTEGER NOT NULL,tbtime TIMESTAMP NOT NULL,typeid INTEGER NOT NULL,sensor CHAR(1) DEFAULT '0',level INTEGER DEFAULT 0,corrected FLOAT NOT NULL,controlinfo CHAR(16) DEFAULT '0000000000000000',useinfo CHAR(16) DEFAULT '0000000000000000',cfailed TEXT DEFAULT NULL,UNIQUE ( stationid, obstime, paramid, level, sensor, typeid ));"
+sqlite3 $1 "CREATE TABLE data (stationid INTEGER NOT NULL,obstime TIMESTAMP NOT NULL,original FLOAT NOT NULL,paramid INTEGER NOT NULL,tbtime TIMESTAMP NOT NULL,typeid INTEGER NOT NULL,sensor CHAR(1) DEFAULT '0',level INTEGER DEFAULT 0,corrected FLOAT NOT NULL,controlinfo CHAR(16) DEFAULT '0000000000000000',useinfo CHAR(16) DEFAULT '0000000000000000',cfailed TEXT DEFAULT NULL,UNIQUE ( stationid, obstime, paramid, level, sensor, typeid ));"
