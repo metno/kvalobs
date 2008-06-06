337cb69f87472f953762ad536e775ef9e4d60d6e
Børge Moe <borge.moe@met.no>
2008-06-06 08:45:57 +0000

    - Add predefined roles kv_admin, kv_read, kv_write and kvproc. - Changes the schema definitions to grant privileges to all tables.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@426 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ src/kvalobs_database/kvalobs_roles.sql
@@ -0,0 +1,33 @@
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- 
+-- kvalobs - quality assurance system for meteorological observations.
+--
+-- Copyright (C) 2007 met.no
+--
+--  Contact information:
+--  Norwegian Meteorological Institute
+--  Box 43 Blindern
+--  0313 OSLO
+--  NORWAY
+--  E-mail: kvoss@met.no
+--
+--  This is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+-- Kvalobs admin role
+CREATE ROLE kv_admin NOLOGIN SUPERUSER;
+
+-- Kvalobs read/write/delete
+CREATE ROLE kv_write NOLOGIN;
+
+--Kvalobs read only role.
+CREATE ROLE kv_read NOLOGIN;
+
+--A role to be used only by the kvalobs software
+--Set a default password. This shuold be altered by an administrator.
+CREATE ROLE kvproc INHERIT LOGIN PASSWORD 'kvproc' IN ROLE kv_write ;
+
--- src/kvalobs_database/kvalobs_schema.sql
+++ src/kvalobs_database/kvalobs_schema.sql
@@ -1,3 +1,24 @@
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- 
+-- kvalobs - quality assurance system for meteorological observations.
+--
+-- Copyright (C) 2007 met.no
+--
+--  Contact information:
+--  Norwegian Meteorological Institute
+--  Box 43 Blindern
+--  0313 OSLO
+--  NORWAY
+--  E-mail: kvoss@met.no
+--
+--  This is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 CREATE TABLE data (
 	stationid   INTEGER NOT NULL,
 	obstime     TIMESTAMP NOT NULL,
@@ -16,6 +37,12 @@ CREATE TABLE data (
 
 CREATE INDEX data_obstime_index ON data (obstime);
 CREATE INDEX data_tbtime_index ON data (tbtime);
+
+REVOKE ALL ON data FROM public;
+GRANT ALL ON data TO kv_admin;
+GRANT SELECT ON data TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON data TO kv_write;
+
 
 --
 -- This table maintains a complete history of everything that has happened to 
@@ -40,6 +67,11 @@ CREATE TABLE data_history (
 	cfailed     TEXT DEFAULT NULL,    	
 	UNIQUE ( version, stationid, obstime, paramid, level, sensor, typeid ) 
 );
+REVOKE ALL ON data_history FROM public;
+GRANT ALL ON data_history TO kv_admin;
+GRANT SELECT ON data_history TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON data_history TO kv_write;
+
 
 
 CREATE LANGUAGE plpgsql;
@@ -95,6 +127,11 @@ CREATE TABLE text_data (
 	UNIQUE ( stationid, obstime, paramid, typeid ) 
 );
 
+REVOKE ALL ON text_data FROM public;
+GRANT ALL ON text_data TO kv_admin;
+GRANT SELECT ON text_data TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON text_data TO kv_write;
+
 
 --
 -- This table maintains a complete history of everything that has happened to 
@@ -113,6 +150,11 @@ CREATE TABLE text_data_history (
 	typeid	    INTEGER NOT NULL,
 	UNIQUE ( version, stationid, obstime, paramid, typeid ) 
 );
+REVOKE ALL ON text_data_history FROM public;
+GRANT ALL ON text_data_history TO kv_admin;
+GRANT SELECT ON text_data_history TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON text_data_history TO kv_write;
+
 
 --
 -- Trigger function for propagating changes to the text_data table into text_data_history
@@ -171,6 +213,8 @@ WHERE (data.stationid NOT IN ( SELECT text_data.stationid FROM text_data )) OR
       (data.obstime NOT IN ( SELECT text_data.obstime FROM text_data ));
 
 
+
+
 CREATE VIEW text_data_view AS
 SELECT text_data.stationid, text_data.obstime, text_data.original, text_data.paramid, text_data.tbtime, text_data.typeid
 FROM text_data
@@ -187,6 +231,11 @@ CREATE TABLE rejectdecode (
 	UNIQUE ( tbtime, message, decoder )
 );
 
+REVOKE ALL ON rejectdecode FROM public;
+GRANT ALL ON rejectdecode TO kv_admin;
+GRANT SELECT ON rejectdecode TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON rejectdecode TO kv_write;
+
 
 CREATE TABLE model_data (
 	stationid INTEGER NOT NULL,
@@ -197,6 +246,11 @@ CREATE TABLE model_data (
 	original  FLOAT DEFAULT NULL,  
 	UNIQUE ( stationid, obstime, paramid, level, modelid )
 );
+REVOKE ALL ON model_data FROM public;
+GRANT ALL ON model_data TO kv_admin;
+GRANT SELECT ON model_data TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON model_data TO kv_write;
+
 
 
 CREATE TABLE model (
@@ -205,6 +259,13 @@ CREATE TABLE model (
 	comment TEXT DEFAULT NULL,
 	UNIQUE ( modelid )
 );
+
+REVOKE ALL ON model FROM public;
+GRANT ALL ON model TO kv_admin;
+GRANT SELECT ON model TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON model TO kv_write;
+
+
 
 
 CREATE TABLE types (
@@ -218,12 +279,23 @@ CREATE TABLE types (
 	UNIQUE ( typeid )
 );
 
+REVOKE ALL ON types FROM public;
+GRANT ALL ON types TO kv_admin;
+GRANT SELECT ON types TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON types TO kv_write;
+
+
 
 CREATE TABLE generated_types (
 	stationid INTEGER NOT NULL,
 	typeid  INTEGER NOT NULL,
 	UNIQUE ( stationid, typeid )
 );
+REVOKE ALL ON generated_types FROM public;
+GRANT ALL ON generated_types TO kv_admin;
+GRANT SELECT ON generated_types TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON generated_types TO kv_write;
+
 
 
 CREATE TABLE param (
@@ -235,6 +307,11 @@ CREATE TABLE param (
 	comment TEXT DEFAULT NULL,
 	UNIQUE ( paramid )
 );
+
+REVOKE ALL ON param FROM public;
+GRANT ALL ON param TO kv_admin;
+GRANT SELECT ON param TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON param TO kv_write;
 
 
 CREATE TABLE station (
@@ -255,6 +332,11 @@ CREATE TABLE station (
 	UNIQUE ( stationid, fromtime )
 );
 
+REVOKE ALL ON station FROM public;
+GRANT ALL ON station TO kv_admin;
+GRANT SELECT ON station TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON station TO kv_write;
+
 
 
 
@@ -269,6 +351,11 @@ CREATE TABLE checks (
 	fromtime TIMESTAMP NOT NULL,
 	UNIQUE ( stationid, qcx, language, fromtime )
 );
+REVOKE ALL ON checks FROM public;
+GRANT ALL ON checks TO kv_admin;
+GRANT SELECT ON checks TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON checks TO kv_write;
+
 
 
 CREATE TABLE station_param (
@@ -286,6 +373,10 @@ CREATE TABLE station_param (
 	UNIQUE ( stationid, paramid, level, sensor, fromday, today, hour, qcx, fromtime )
 );
 
+REVOKE ALL ON station_param FROM public;
+GRANT ALL ON station_param TO kv_admin;
+GRANT SELECT ON station_param TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON station_param TO kv_write;
 
 CREATE TABLE algorithms (
 	language  INTEGER DEFAULT NULL,
@@ -294,6 +385,12 @@ CREATE TABLE algorithms (
 	script TEXT NOT NULL,
 	UNIQUE ( checkname, language )
 );
+REVOKE ALL ON algorithms FROM public;
+GRANT ALL ON algorithms TO kv_admin;
+GRANT SELECT ON algorithms TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON algorithms TO kv_write;
+
+
 
 
 CREATE TABLE reference_station (
@@ -302,6 +399,11 @@ CREATE TABLE reference_station (
 	reference TEXT DEFAULT NULL,
 	UNIQUE ( stationid, paramsetid ) 
 );
+REVOKE ALL ON reference_station FROM public;
+GRANT ALL ON reference_station TO kv_admin;
+GRANT SELECT ON reference_station TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON reference_station TO kv_write;
+
 
 
 CREATE TABLE timecontrol (
@@ -312,6 +414,10 @@ CREATE TABLE timecontrol (
 	qcx       TEXT NOT NULL,
 	UNIQUE ( fromday, today, time, priority )  
 );
+REVOKE ALL ON timecontrol FROM public;
+GRANT ALL ON timecontrol TO kv_admin;
+GRANT SELECT ON timecontrol TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON timecontrol TO kv_write;
 
 
 CREATE TABLE obs_pgm (
@@ -358,12 +464,23 @@ CREATE TABLE obs_pgm (
 );
 
 CREATE INDEX obs_pgm_index_fromtime_totime_typeid_stationid ON obs_pgm (fromtime, totime, typeid, stationid);
+REVOKE ALL ON obs_pgm FROM public;
+GRANT ALL ON obs_pgm TO kv_admin;
+GRANT SELECT ON obs_pgm TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON obs_pgm TO kv_write;
+
+
 
 CREATE TABLE operator (
 	username TEXT NOT NULL,
 	userid	 INTEGER NOT NULL,
 	UNIQUE (username)
 );
+REVOKE ALL ON operator FROM public;
+GRANT ALL ON operator TO kv_admin;
+GRANT SELECT ON operator TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON operator TO kv_write;
+
 
 
 CREATE TABLE qcx_info (
@@ -373,6 +490,10 @@ CREATE TABLE qcx_info (
         comment	    TEXT DEFAULT NULL,
 	UNIQUE (medium_qcx)
 );
+REVOKE ALL ON qcx_info FROM public;
+GRANT ALL ON qcx_info TO kv_admin;
+GRANT SELECT ON qcx_info TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON qcx_info TO kv_write;
 
 
 CREATE TABLE key_val (
@@ -381,6 +502,11 @@ CREATE TABLE key_val (
 	val   	TEXT,
 	UNIQUE (package, key)
 ); 
+REVOKE ALL ON key_val FROM public;
+GRANT ALL ON key_val TO kv_admin;
+GRANT SELECT ON key_val TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON key_val TO kv_write;
+
 
 
 CREATE TABLE stationid_klima (
@@ -389,6 +515,11 @@ CREATE TABLE stationid_klima (
         klop     INTEGER DEFAULT NULL,
         UNIQUE ( stationid )
 );
+REVOKE ALL ON stationid_klima FROM public;
+GRANT ALL ON stationid_klima TO kv_admin;
+GRANT SELECT ON stationid_klima TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON stationid_klima TO kv_write;
+
 
 
 CREATE TABLE param_feltfil (
@@ -400,6 +531,10 @@ CREATE TABLE param_feltfil (
 	feltfil_level2  INTEGER DEFAULT NULL,
 	UNIQUE ( paramid, level, feltfil_vertical_coordinate )
 );
+REVOKE ALL ON param_feltfil FROM public;
+GRANT ALL ON param_feltfil TO kv_admin;
+GRANT SELECT ON param_feltfil TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON param_feltfil TO kv_write;
 
 
 
@@ -419,6 +554,11 @@ CREATE TABLE workque (
 
 CREATE INDEX workque_index_priority_stationid_typeid_obstime ON
 workque (priority, stationid, typeid, obstime);
+REVOKE ALL ON workque FROM public;
+GRANT ALL ON workque TO kv_admin;
+GRANT SELECT ON workque TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON workque TO kv_write;
+
 
 
 CREATE TABLE workstatistik  (
@@ -434,6 +574,10 @@ CREATE TABLE workstatistik  (
        service_stop  TIMESTAMP ,
        UNIQUE(stationid, obstime, typeid)
 );
+REVOKE ALL ON workstatistik FROM public;
+GRANT ALL ON workstatistik TO kv_admin;
+GRANT SELECT ON workstatistik TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON workstatistik TO kv_write;
 
 
 CREATE TABLE ps_subscribers  (
@@ -445,3 +589,8 @@ CREATE TABLE ps_subscribers  (
        created  TIMESTAMP NOT NULL,
        UNIQUE(name)
 );
+
+REVOKE ALL ON ps_subscribers FROM public;
+GRANT ALL ON ps_subscribers TO kv_admin;
+GRANT SELECT ON ps_subscribers TO kv_read;
+GRANT SELECT, UPDATE, INSERT ON ps_subscribers TO kv_write;
