b5aca7f95f8b095eac5a85e7a062a6f32a71e7c8
Børge Moe <borge.moe@met.no>
2009-11-23 08:42:17 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@996 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvDataInputd/kvdataclt.cc
+++ src/kvDataInputd/kvdataclt.cc
@@ -28,6 +28,8 @@
   with KVALOBS; if not, write to the Free Software Foundation Inc., 
   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
