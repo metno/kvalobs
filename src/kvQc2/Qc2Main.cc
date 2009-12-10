0d6613df7b66b44ca3d448b492d5bb7de0b147de
Paul Emils Eglitis <paule@met.no>
2009-12-10 23:23:21 +0000

    aligned pidfile usage with the other kvalobs programs
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1012 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvQc2/Qc2Main.cc
+++ src/kvQc2/Qc2Main.cc
@@ -29,6 +29,8 @@
   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <boost/thread/thread.hpp>
+#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/exception.hpp>
 #include "Qc2App.h"
 #include <milog/milog.h>
 #include "InitLogger.h"
@@ -38,6 +40,8 @@
 #include <fileutil/pidfileutil.h>
 #include "Qc2Thread.h"
 #include <puTools/miTime>
+#include <kvalobs/kvPath.h>
+
 
 
 //#include <qt.h>
@@ -79,7 +83,7 @@ main( int argc, char** argv )
   string constr( KvApp::createConnectString() );
   char *pKv = getenv( "KVALOBS" );
   bool error;
-  string pidfile;
+  //string pidfile;
 
   milog::LogContext logContext("Qc2 ...");
 
@@ -114,35 +118,71 @@ main( int argc, char** argv )
 
   LOGINFO( "Qc2: starting ...." );
 
-  if ( !pKv )
-  {
-    LOGFATAL( "The environment variable KVALOBS must be set!" );
+  filesystem::path rundir( kvPath("localstatedir") + "/run" );
+  if ( ! boost::filesystem::exists(rundir) ) {
+    try {
+      filesystem::create_directories(rundir);      
+    }
+    catch ( filesystem::filesystem_error & e) {
+      LOGFATAL( e.what() );
+      return 1;
+    }
+  }
+  else if ( ! filesystem::is_directory(rundir) ) {
+    LOGFATAL( rundir.native_file_string() << "exists but is not a directory" );
     return 1;
   }
+  filesystem::path pidfile( dnmi::file::createPidFileName( rundir.native_file_string(), "kvQc2" ) );
 
-  pidfile = string( pKv ) + "/var/run/Qc2.pid";
-
-  std::cout << pidfile << std::endl;
-
-  if ( dnmi::file::isRunningPidFile( pidfile, error ) )
+  if ( dnmi::file::isRunningPidFile( pidfile.native_file_string(), error ) )
   {
     if ( error )
     {
       LOGFATAL( "An error occured while reading the pidfile:" << endl
-                << pidfile << " remove the file if it exist and"
-                << endl << "Qc2 is not running. " <<
-                "If it is running and there is problems. Kill Qc2 and"
+                << pidfile.native_file_string() << " remove the file if it exist and"
+                << endl << "kvQc2 is not running. " <<
+                "If it is running and there is problems. Kill kvQc2 and"
                 << endl << "restart it." << endl << endl );
       return 1;
     }
     else
     {
-      LOGFATAL( "Is Qc2 allready running?" << endl
-                << "If not remove the pidfile: " << pidfile );
+      LOGFATAL( "Is kvQc2 allready running?" << endl
+                << "If not remove the pidfile: " << pidfile.native_file_string() );
       return 1;
     }
   }
 
+
+  //if ( !pKv )
+  //{
+    //LOGFATAL( "The environment variable KVALOBS must be set!" );
+    //return 1;
+  //}
+//
+  //pidfile = string( pKv ) + "/var/run/Qc2.pid";
+//
+  //std::cout << pidfile << std::endl;
+//
+  //if ( dnmi::file::isRunningPidFile( pidfile, error ) )
+  //{
+    //if ( error )
+    //{
+      //LOGFATAL( "An error occured while reading the pidfile:" << endl
+                //<< pidfile << " remove the file if it exist and"
+                //<< endl << "Qc2 is not running. " <<
+                //"If it is running and there is problems. Kill Qc2 and"
+                //<< endl << "restart it." << endl << endl );
+      //return 1;
+    //}
+    //else
+    //{
+      //LOGFATAL( "Is Qc2 allready running?" << endl
+                //<< "If not remove the pidfile: " << pidfile );
+      //return 1;
+    //}
+  //}
+//
 
   Qc2App app( argc, argv, dbdriver, constr, options );
   
@@ -155,7 +195,7 @@ main( int argc, char** argv )
   orb = app.getOrb();
   poa = app.getPoa();
  
-  app.createPidFile( "Qc2" );
+  app.createPidFile( "kvQc2" );
   sleep(1);
 
   Qc2Work Qc2Work( app, htmlpath );    //commented out while I test program options !!!!
