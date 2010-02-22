9be4b7ceb9275d33c38a420829eab5768ed39dc2
Børge Moe <borge.moe@met.no>
2010-02-22 10:56:35 +0000

    Testcase for decoding of x in the synop code.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1124 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/lib/decoder/synopdecoder/test/kvSynopDecodeTest.cc
+++ src/lib/decoder/synopdecoder/test/kvSynopDecodeTest.cc
@@ -30,13 +30,69 @@
 */
 
 
+#include <limits.h>
+#include <string>
+#include <kvalobs/kvData.h>
+#include <list>
+#include <kvalobs/kvStation.h>
+#include "kvSynopDecoder.h"
+
 #include <gtest/gtest.h>
+
+using namespace std;
 
 TEST( SynopDecode, hVVN_missing )
 {
 
 }
 
+/**
+ * Test decoding of x. x should be interpreted as / in the synop, ie not set.
+ */
+TEST( SynopDecode, decodeX )
+{
+	list<kvalobs::kvStation> stationList;
+	list<kvalobs::kvData> dataList;
+	kvalobs::kvRejectdecode rejectInfo;
+	kvSynopDecoder synopDecoder;
+
+	/*
+	 *     kvStation( int st, float la, float lo, float he, float max,
+	       const miutil::miString& na, int wm, int nn,
+	       const miutil::miString& ic, const miutil::miString& ca,
+	       const miutil::miString& ss, int environmentid,
+	       bool static_, const miutil::miTime& fromtime)
+	 *
+	 */
+	stationList.push_back( kvalobs::kvStation(76900, 66, 2, 6, 0,"MIKE", 0, 76900, "", "LDWR","",7, true,
+			                                  miutil::miTime("1977-01-01 00:00:00")));
+
+	ASSERT_TRUE( synopDecoder.initialise( stationList, 10, 20 ) ) << "Cant initialize the synopdecoder.";
+	string shipmsg="BBXX LDWR 07221 99662 10018 41997 02718 10036 21036 40067 53095 70111 8xx//"
+			       " 22200 04064 11016 3//// 4//// 5//// 70082=";
+
+	bool decoded = synopDecoder.decode( shipmsg , dataList );
+
+	EXPECT_TRUE( decoded ) << "Failed to decode ship message.";
+
+	if( ! decoded )  {
+		rejectInfo = synopDecoder.rejected("synop");
+		cerr << "Rejected: " << rejectInfo << endl;
+	}
+
+	int Nh=INT_MAX; //paramid 14
+	int Cl=INT_MAX; //paramid 23
+
+	for( list<kvalobs::kvData>::iterator it=dataList.begin(); it != dataList.end(); ++it ) {
+		if( it->paramID() == 14 )
+			Nh = static_cast<int>(it->original());
+		else if( it->paramID() == 23 )
+			Cl = static_cast<int>( it->original() );
+	}
+
+	EXPECT_TRUE( Nh == INT_MAX ) << "Nh is invalid set.";
+	EXPECT_TRUE( Cl == INT_MAX ) << "Cl is invalid set.";
+}
 
 
 int
