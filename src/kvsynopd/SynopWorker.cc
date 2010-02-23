1fb70e6f423440914d1ed4dc22ac3429bfa3d172
Børge Moe <borge.moe@met.no>
2010-02-23 21:04:46 +0000

    Implement the method loadSynopData to use the new function loadSynopData.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1138 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvsynopd/SynopWorker.cc
+++ src/kvsynopd/SynopWorker.cc
@@ -46,6 +46,7 @@
 #include "obsevent.h"
 #include "synop.h"
 #include <kvalobs/kvPath.h>
+#include "LoadSynopData.h"
 
 using namespace std;
 using namespace kvalobs;
@@ -669,60 +670,7 @@ SynopWorker::loadSynopData(const DataEntryList &dl,
 			   SynopDataList       &sd, 
 			   StationInfoPtr      info)const
 {
-  StationInfo::TLongList          types;
-  StationInfo::RITLongList        itt;
-  DataEntryList::CITDataEntryList it;
-  DataListEntry::ITDataList       itd;
-  DataListEntry::TDataList        dataList;
-
-  sd.clear();
-
-  types=info->typepriority();
-  it=dl.begin();
-
-  for(; it!=dl.end(); it++){
-    SynopData synopData;
-
-    itt=types.rbegin();
-
-    for(;itt!=types.rend(); itt++){
-      dataList=it->getTypeId(*itt);
-      
-      if(dataList.empty())
-	continue;
-
-      itd=dataList.begin();
-      synopData.time(itd->obstime());
-      
-      for(;itd!=dataList.end(); itd++){
-	//COMMENT:
-	//We use only the default sensor for every parameter, ie sensor=0.
-	//This may be to restrective. Shuld this be a configuration option for
-	//the parameters we wish to overide tis behavior for.
-	
-	if(itd->sensor()==0 && itd->level()==0){
-	  synopData.setData(itd->paramID(), itd->original());
-	}else{
-	  LOGINFO("loadSynop: sensor=" << itd->sensor() << " level=" << itd->level()
-		  << " not used!");
-	}
-      }
-    }
-    
-    if(!synopData.undef()){
-      synopData.cleanUpSlash();
-      
-      try{
-	sd[synopData.time()]=synopData;
-      }
-      catch(out_of_range &ex){
-	LOGDEBUG("EXCEPTION(out_of_range): Should not happend!!!"<< endl);
-      }
-      catch(...){
-	LOGDEBUG("EXCEPTION(Unknown): Should never happend!" << endl);
-      }
-    }
-  }
+	loadSynopData( dl, sd, info );
 }
 
 
