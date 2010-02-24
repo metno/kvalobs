2c0277eb88bb21a5cff3062265cf00690b5faf98
Børge Moe <borge.moe@met.no>
2010-02-24 06:31:48 +0000

    Fix indent.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1143 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvsynopd/DataList.h
+++ src/kvsynopd/DataList.h
@@ -39,143 +39,143 @@
 
 
 class DataListEntry{
- 	public:
-  		typedef std::list<Data>                   TDataList;
-  		typedef std::list<Data>::iterator        ITDataList;
-  		typedef std::list<Data>::const_iterator CITDataList;
+public:
+	typedef std::list<Data>                   TDataList;
+	typedef std::list<Data>::iterator        ITDataList;
+	typedef std::list<Data>::const_iterator CITDataList;
 
-  		typedef std::map<int, TDataList>                    TTypeList;
- 	 	typedef std::map<int, TDataList>::iterator         ITTypeList;
-  		typedef std::map<int, TDataList>::const_iterator  CITTypeList;
+	typedef std::map<int, TDataList>                    TTypeList;
+	typedef std::map<int, TDataList>::iterator         ITTypeList;
+	typedef std::map<int, TDataList>::const_iterator  CITTypeList;
 
-  		class TimeError : public std::exception{
-  			public:
-    			TimeError()throw(){};
-    			~TimeError()throw(){}
-    			const char* what()const throw() { return "Timestamp exist!";}
-  		};
- 
-
-
- 	private:
-  		miutil::miTime obsTime_;
-  		TTypeList      dataList;
-
- 	public:
-  		DataListEntry(){}
-
-  		DataListEntry(const DataListEntry &de)
-    		:obsTime_(de.obsTime_), dataList(de.dataList){}
-  
-  		DataListEntry(const miutil::miTime &t)
-    		:obsTime_(t){}
-
-  		~DataListEntry(){}
-
-  		DataListEntry& operator=(const DataListEntry &de);
-      
-  		miutil::miTime obstime()const{ return obsTime_;}
-
-  		/**
-   	 * Returns the list of Data elements for typeID.
-   	 */
-  		TDataList getTypeId(int typeID)const;
-
-  		/**
-  		 * Return all typeID's for this list.
-  		 */
-  		std::list<int> getTypes()const;
+	class TimeError : public std::exception{
+	public:
+		TimeError()throw(){};
+		~TimeError()throw(){}
+		const char* what()const throw() { return "Timestamp exist!";}
+	};
 
 
-  		/**
-   	 * size, returns the number of Data elements for typeID.
-   	 */
-  		int size(int typeID)const;
+
+private:
+	miutil::miTime obsTime_;
+	TTypeList      dataList;
+
+public:
+	DataListEntry(){}
+
+	DataListEntry(const DataListEntry &de)
+	:obsTime_(de.obsTime_), dataList(de.dataList){}
+
+	DataListEntry(const miutil::miTime &t)
+	:obsTime_(t){}
+
+	~DataListEntry(){}
+
+	DataListEntry& operator=(const DataListEntry &de);
+
+	miutil::miTime obstime()const{ return obsTime_;}
+
+	/**
+	 * Returns the list of Data elements for typeID.
+	 */
+	TDataList getTypeId(int typeID)const;
+
+	/**
+	 * Return all typeID's for this list.
+	 */
+	std::list<int> getTypes()const;
 
 
-  		/**
-       * Returns the total number of elements in this DetaListEntry.
-   	 */
-  		int size()const;
+	/**
+	 * size, returns the number of Data elements for typeID.
+	 */
+	int size(int typeID)const;
 
-  		/**
-   	 * add, put the Data, d, in a list with data of typeID.
-   	 * If this is the first data that is added to the entry
-   	 * the obsTime is set to be the obstime of data.obstime().
-   	 * All subsecuent adds must have the same obstime, if not an 
-   	 * TimeErrorException is raised. 
-   	 *
-   	 * \return true if the data was successful added. false
-   	 *         if there allready is a element of data in the entry.
-   	 *
-   	 * \exception TimeError if the obstime field dont match
-   	 *            the obsTime_ field of this entry.
-   	 */
-  		void add(const Data &d);
-  		
-  		friend std::ostream& operator<<(std::ostream& ost,
-			   									 const DataListEntry& dl);
-  		
+
+	/**
+	 * Returns the total number of elements in this DetaListEntry.
+	 */
+	int size()const;
+
+	/**
+	 * add, put the Data, d, in a list with data of typeID.
+	 * If this is the first data that is added to the entry
+	 * the obsTime is set to be the obstime of data.obstime().
+	 * All subsecuent adds must have the same obstime, if not an
+	 * TimeErrorException is raised.
+	 *
+	 * \return true if the data was successful added. false
+	 *         if there allready is a element of data in the entry.
+	 *
+	 * \exception TimeError if the obstime field dont match
+	 *            the obsTime_ field of this entry.
+	 */
+	void add(const Data &d);
+
+	friend std::ostream& operator<<(std::ostream& ost,
+			const DataListEntry& dl);
+
 };
 
 
 class DataEntryList{
- 	public:
-  		typedef std::list<DataListEntry>                   TDataEntryList;
-  		typedef std::list<DataListEntry>::iterator        ITDataEntryList;
-  		typedef std::list<DataListEntry>::const_iterator CITDataEntryList;
+public:
+	typedef std::list<DataListEntry>                   TDataEntryList;
+	typedef std::list<DataListEntry>::iterator        ITDataEntryList;
+	typedef std::list<DataListEntry>::const_iterator CITDataEntryList;
 
- 	private:
-  		TDataEntryList dataList;
+private:
+	TDataEntryList dataList;
 
- 	public:
-  		DataEntryList();
-  		DataEntryList(const DataEntryList &d);
-  		~DataEntryList();  
-  
-  		DataEntryList& operator=(const DataEntryList &rhs);
+public:
+	DataEntryList();
+	DataEntryList(const DataEntryList &d);
+	~DataEntryList();
 
-  		void clear(){ dataList.clear();}
+	DataEntryList& operator=(const DataEntryList &rhs);
 
-
-  		/**
-   	 * Number of continues times from the obstime this data set represent.
-   	 */
-  		int nContinuesTimes()const;
-
-  		/**
-   	 * \brief check if we have continues times for the typeids in the
-   	 *        list \a ctList.
-   	 *
-   	 * \param ctList check continues times for the typeid in this list.
-   	 * \param hours return true if we have \a hours with continues times
-   	 *              for all types in \a ctList. False otherwise.
-   	 */
-  		bool hasContinuesTimes(const std::list<int> &ctList, int hours)const; 
+	void clear(){ dataList.clear();}
 
 
-	  	/**
-	    * Insert Data at timeIndex (d.obstime()) in the list. If the data (d),
-	    * exist it is replaced.
-	    *
-	    * \param timeIndex, the time the Data (d) is for.
-	    * \param d the Data to insert at timeIndex.
-	    */
-  		void insert(const Data &d);
+	/**
+	 * Number of continues times from the obstime this data set represent.
+	 */
+	int nContinuesTimes()const;
 
-  		int  size()const { return dataList.size();}
+	/**
+	 * \brief check if we have continues times for the typeids in the
+	 *        list \a ctList.
+	 *
+	 * \param ctList check continues times for the typeid in this list.
+	 * \param hours return true if we have \a hours with continues times
+	 *              for all types in \a ctList. False otherwise.
+	 */
+	bool hasContinuesTimes(const std::list<int> &ctList, int hours)const;
 
-  		ITDataEntryList  find(const miutil::miTime &from);
-  		CITDataEntryList find(const miutil::miTime &from)const;    
- 
-  		ITDataEntryList  begin(){ return dataList.begin();}
-  		CITDataEntryList begin()const{ return dataList.begin();}
 
-  		ITDataEntryList  end(){ return dataList.end();}
-  		CITDataEntryList end()const { return dataList.end();}
+	/**
+	 * Insert Data at timeIndex (d.obstime()) in the list. If the data (d),
+	 * exist it is replaced.
+	 *
+	 * \param timeIndex, the time the Data (d) is for.
+	 * \param d the Data to insert at timeIndex.
+	 */
+	void insert(const Data &d);
 
-  		friend std::ostream& operator<<(std::ostream& ost,
-				  									 const DataEntryList& sd);
+	int  size()const { return dataList.size();}
+
+	ITDataEntryList  find(const miutil::miTime &from);
+	CITDataEntryList find(const miutil::miTime &from)const;
+
+	ITDataEntryList  begin(){ return dataList.begin();}
+	CITDataEntryList begin()const{ return dataList.begin();}
+
+	ITDataEntryList  end(){ return dataList.end();}
+	CITDataEntryList end()const { return dataList.end();}
+
+	friend std::ostream& operator<<(std::ostream& ost,
+			const DataEntryList& sd);
 };
 
 
