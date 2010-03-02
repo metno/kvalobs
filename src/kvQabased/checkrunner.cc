516090fa4d0634ffa63a79df7d04ce4a585135bd
Vegard Bønes <vegard.bones@met.no>
2010-03-02 12:52:44 +0000

    reformatted source - no other changes
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1209 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvQabased/checkrunner.cc
+++ src/kvQabased/checkrunner.cc
@@ -1,33 +1,33 @@
 /*
-  Kvalobs - Free Quality Control Software for Meteorological Observations
+ Kvalobs - Free Quality Control Software for Meteorological Observations
 
-  $Id: checkrunner.cc,v 1.1.2.8 2007/09/27 09:02:21 paule Exp $
+ $Id: checkrunner.cc,v 1.1.2.8 2007/09/27 09:02:21 paule Exp $
 
-  Copyright (C) 2007 met.no
+ Copyright (C) 2007 met.no
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: kvalobs-dev@met.no
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: kvalobs-dev@met.no
 
-  This file is part of KVALOBS
+ This file is part of KVALOBS
 
-  KVALOBS is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+ KVALOBS is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-  KVALOBS is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
+ KVALOBS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along
-  with KVALOBS; if not, write to the Free Software Foundation Inc.,
-  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License along
+ with KVALOBS; if not, write to the Free Software Foundation Inc.,
+ 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #include "kvPerlParser.h"
 #undef list
 
@@ -60,306 +60,328 @@ using boost::filesystem::path;
 
 namespace
 {
-  enum LogType { Debug, Info, Warn, Error };
+enum LogType
+{
+	Debug, Info, Warn, Error
+};
 
-  inline void log_( const std::string & msg, LogType t = Info )
-  {
-    switch ( t )
-    {
-      case Debug:
-        IDLOGDEBUG( "html", msg << endl );
-        break;
-      case Info:
-        IDLOGINFO( "html", msg << endl );
-        break;
-      case Warn:
-        IDLOGWARN( "html", msg << endl );
-        break;
-      case Error:
-        IDLOGERROR( "html", msg << endl );
-        break;
-    }
-  }
-
-  void logEnd_( const std::string & msg, LogType t = Info )
-  {
-    log_( msg, t );
-    IDLOGINFO( "html", "</PRE></CODE>" );
-    Logger::removeLogger( "html" );
-    if ( t == Error )
-      throw std::runtime_error( msg );
-  }
+inline void log_(const std::string & msg, LogType t = Info)
+{
+	switch (t)
+	{
+	case Debug:
+		IDLOGDEBUG( "html", msg << endl )
+		;
+		break;
+	case Info:
+		IDLOGINFO( "html", msg << endl )
+		;
+		break;
+	case Warn:
+		IDLOGWARN( "html", msg << endl )
+		;
+		break;
+	case Error:
+		IDLOGERROR( "html", msg << endl )
+		;
+		break;
+	}
 }
 
-CheckRunner::CheckRunner( const kvStationInfo & params,
-    kvQABaseDBConnection & con_,
-    const path & logp )
-    : stinfo( params )
-    , dbcon( con_ )
-    , meteod( con_, params )
-    , logpath_( logp )
-    , checkCreator_(meteod, params, con_)
+void logEnd_(const std::string & msg, LogType t = Info)
 {
-  if ( !dbcon.dbOk() )
-    throw runtime_error( "No database connection" );
+	log_(msg, t);
+	IDLOGINFO( "html", "</PRE></CODE>" );
+	Logger::removeLogger("html");
+	if (t == Error)
+		throw std::runtime_error(msg);
+}
+}
+
+CheckRunner::CheckRunner(const kvStationInfo & params,
+		kvQABaseDBConnection & con_, const path & logp) :
+	stinfo(params), dbcon(con_), meteod(con_, params), logpath_(logp),
+			checkCreator_(meteod, params, con_)
+{
+	if (!dbcon.dbOk())
+		throw runtime_error("No database connection");
 }
 
 CheckRunner::~CheckRunner()
-{}
+{
+}
 
 bool CheckRunner::shouldProcess()
 {
-  // Skip stations with typeid < 0 (aggregated values)
-  if ( stinfo.typeID() < 0 ) {
-    log_( "Incoming data are agregated" );
-    return false;
-  }
+	// Skip stations with typeid < 0 (aggregated values)
+	if (stinfo.typeID() < 0)
+	{
+		log_("Incoming data are agregated");
+		return false;
+	}
 
-  // make sure we are supposed to run checks for this station
-  // - if station not in obs_pgm: skip it!
-  // ...unless this is an unknown ship, stinfo.stationID()>10000000
-  if ( stinfo.stationID() <= 10000000 )
-  {
-    if ( checkCreator_.oprogramlist().empty() ) {
-      log_( "Station does not exist in obspgm" );
-      return false;
-    }
-  }
-  if ( hqcCorrected() ) {
-    log_( "Incoming data has been controlled by HQC" );
-    return false;
-  }
-  if ( dataWasCheckedBefore() ) {
-    log_( "Checks have been run before on this data" );
-    return false;
-  }
+	// make sure we are supposed to run checks for this station
+	// - if station not in obs_pgm: skip it!
+	// ...unless this is an unknown ship, stinfo.stationID()>10000000
+	if (stinfo.stationID() <= 10000000)
+	{
+		if (checkCreator_.oprogramlist().empty())
+		{
+			log_("Station does not exist in obspgm");
+			return false;
+		}
+	}
+	if (hqcCorrected())
+	{
+		log_("Incoming data has been controlled by HQC");
+		return false;
+	}
+	if (dataWasCheckedBefore())
+	{
+		log_("Checks have been run before on this data");
+		return false;
+	}
 
-  return true;
+	return true;
 }
 
 // Return if any data has been modified by HQC
 bool CheckRunner::hqcCorrected()
 {
-  const kvQABaseMeteodata::DataFromTime & data_ = meteod.getData();
+	const kvQABaseMeteodata::DataFromTime & data_ = meteod.getData();
 
-  for ( kvQABaseMeteodata::DataFromTime::const_iterator it = data_.begin(); it != data_.end(); ++ it )
-  {
-    typedef kvQABaseMeteodata::obs_data KvDL;
-    const KvDL & dl = it->second;
-    for ( KvDL::const_iterator itb = dl.begin(); itb != dl.end(); ++ itb )
-      if ( itb->typeID() == stinfo.typeID() and itb->controlinfo().flag( 15 ) )
-        return true;
-  }
-  return false;
+	for (kvQABaseMeteodata::DataFromTime::const_iterator it = data_.begin(); it
+			!= data_.end(); ++it)
+	{
+		typedef kvQABaseMeteodata::obs_data KvDL;
+		const KvDL & dl = it->second;
+		for (KvDL::const_iterator itb = dl.begin(); itb != dl.end(); ++itb)
+			if (itb->typeID() == stinfo.typeID()
+					and itb->controlinfo().flag(15))
+				return true;
+	}
+	return false;
 }
 
 bool CheckRunner::dataWasCheckedBefore()
 {
-  const kvQABaseMeteodata::DataFromTime & data_ = meteod.getData();
+	const kvQABaseMeteodata::DataFromTime & data_ = meteod.getData();
 
-  if ( data_.empty() ) // no data to check
-    return false;
+	if (data_.empty()) // no data to check
+		return false;
 
-  for ( kvQABaseMeteodata::DataFromTime::const_iterator it = data_.begin(); it != data_.end(); ++ it )
-  {
-    typedef kvQABaseMeteodata::obs_data KvDL;
-    const KvDL & dl = it->second;
-    for ( KvDL::const_iterator itb = dl.begin(); itb != dl.end(); ++ itb ) {
-//      cout << "Considering: " << * itb << endl;
-      if ( itb->typeID() == stinfo.typeID() and itb->useinfo().flag( 0 ) == 9 )
-        return false;
-    }
-  }
-  return true;
+	for (kvQABaseMeteodata::DataFromTime::const_iterator it = data_.begin(); it
+			!= data_.end(); ++it)
+	{
+		typedef kvQABaseMeteodata::obs_data KvDL;
+		const KvDL & dl = it->second;
+		for (KvDL::const_iterator itb = dl.begin(); itb != dl.end(); ++itb)
+		{
+			//      cout << "Considering: " << * itb << endl;
+			if (itb->typeID() == stinfo.typeID() and itb->useinfo().flag(0)
+					== 9)
+				return false;
+		}
+	}
+	return true;
 }
-
 
 void CheckRunner::updateStaticVariables()
 {
-  //Bxrge Moe
-  //2004.9.21
-  //Have changed how frequent the static tables is updated.
-  //They was updated once for every read. I have changed this
-  //to one time every hour. This mean that we may have wrong data
-  //for at most one hour. We can remedy this by restarting kvalobs.
-  //In the future can we implement a reread by a signal, ex SIGHUP.
-  if ( updateStaticTime.undef() || updateStaticTime < miutil::miTime::nowTime() )
-  {
-    updateStaticTime = miutil::miTime::nowTime();
-    updateStaticTime.addHour( 1 );
+	//Bxrge Moe
+	//2004.9.21
+	//Have changed how frequent the static tables is updated.
+	//They was updated once for every read. I have changed this
+	//to one time every hour. This mean that we may have wrong data
+	//for at most one hour. We can remedy this by restarting kvalobs.
+	//In the future can we implement a reread by a signal, ex SIGHUP.
+	if (updateStaticTime.undef() || updateStaticTime
+			< miutil::miTime::nowTime())
+	{
+		updateStaticTime = miutil::miTime::nowTime();
+		updateStaticTime.addHour(1);
 
-    dbcon.updateStatics();
-  }
+		dbcon.updateStatics();
+	}
 }
 
-void CheckRunner::findChecks( list<kvChecks> & out )
+void CheckRunner::findChecks(list<kvChecks> & out)
 {
-  if ( ! dbcon.getChecks( stinfo.stationID(), stinfo.obstime(), out ) )
-    logEnd_( "CheckRunner: Error reading checks", Error );
+	if (!dbcon.getChecks(stinfo.stationID(), stinfo.obstime(), out))
+		logEnd_("CheckRunner: Error reading checks", Error);
 }
 
-void CheckRunner::runCheck( const std::string & checkScript, const kvalobs::kvChecks & check )
+void CheckRunner::runCheck(const std::string & checkScript,
+		const kvalobs::kvChecks & check)
 {
-  LogContext context( "Running check " + check.checkname() );
+	LogContext context("Running check " + check.checkname());
 
-  log_( "<HR>" );
-  log_( string( "<H2>Check loop, type:" ) + check.qcx() + " name:" + check.checkname() + "</H2>" );
+	log_("<HR>");
+	log_(string("<H2>Check loop, type:") + check.qcx() + " name:"
+			+ check.checkname() + "</H2>");
 
-
-  // Writing the script to be run to html:
-//#define LOG_CHECK_SCRIPT
+	// Writing the script to be run to html:
+	//#define LOG_CHECK_SCRIPT
 #ifdef LOG_CHECK_SCRIPT
-    log_( "Final checkstring:" );
-    log_( "<font color=#007700>" );
-    log_( checkScript );
-    log_( "</font>" );
+	log_( "Final checkstring:" );
+	log_( "<font color=#007700>" );
+	log_( checkScript );
+	log_( "</font>" );
 #endif
 
-  /* ----- run check ---------------------------------- */
-  kvPerlParser parser;                // the perlinterpreter
-  map<string, double> retvalues;
-  if ( ! parser.runScript( checkScript, retvalues ) )
-    return log_( "CheckRunner::runCheck failed in parser.runScript", Error );
+	/* ----- run check ---------------------------------- */
+	kvPerlParser parser; // the perlinterpreter
+	map<string, double> retvalues;
+	if (!parser.runScript(checkScript, retvalues))
+		return log_("CheckRunner::runCheck failed in parser.runScript", Error);
 
-  // TEST --------------------------------------------------
-  log_( "Successfully run check.", Debug );
-  IDLOGDEBUG( "html", "Number of return parameters:" << retvalues.size() << endl );
-  for ( map<string, double>::iterator dp = retvalues.begin(); dp != retvalues.end(); dp++ )
-    IDLOGDEBUG( "html", "Param:" << dp->first << " equals " << dp->second << endl );
-  // ----------------------------------------------------------
+	// TEST --------------------------------------------------
+	log_("Successfully run check.", Debug);
+	IDLOGDEBUG( "html", "Number of return parameters:" << retvalues.size() << endl );
+	for (map<string, double>::iterator dp = retvalues.begin(); dp
+			!= retvalues.end(); dp++)
+		IDLOGDEBUG( "html", "Param:" << dp->first << " equals " << dp->second << endl );
+	// ----------------------------------------------------------
 
-  if ( not retvalues.empty() )
-  {
-    // Update parameters with new control-flags and any return-variables - also update kvStationInfo
-    IDLOGINFO( "html", "Updating observation(s) with return values from check" << endl );
-    if ( ! meteod.updateParameters( retvalues, check ) )
-      log_( "CheckRunner::runCheck failed in updateParameters", Error );
-  }
-  else
-    log_( "No return values from check - skip update..", Warn );
+	if (not retvalues.empty())
+	{
+		// Update parameters with new control-flags and any return-variables - also update kvStationInfo
+		IDLOGINFO( "html", "Updating observation(s) with return values from check" << endl );
+		if (!meteod.updateParameters(retvalues, check))
+			log_("CheckRunner::runCheck failed in updateParameters", Error);
+	}
+	else
+		log_("No return values from check - skip update..", Warn);
 }
 
-void CheckRunner::operator() ( bool forceCheck )
+void CheckRunner::operator()(bool forceCheck)
 {
-  openHTMLStream();
-  try
-  {
-    if ( not forceCheck and not shouldProcess() ) {
-      logEnd_( "Will not process data" );
-      return;
-    }
-    updateStaticVariables();
-  }
-  catch ( std::exception & e )
-  {
-    // We wish to log errors on html:
-    logEnd_( e.what(), Error );
-    throw;
-  }
+	openHTMLStream();
+	try
+	{
+		if (not forceCheck and not shouldProcess())
+		{
+			logEnd_("Will not process data");
+			return;
+		}
+		updateStaticVariables();
+	} catch (std::exception & e)
+	{
+		// We wish to log errors on html:
+		logEnd_(e.what(), Error);
+		throw;
+	}
 
-  //  relevant checks (QC1) for this observation
-  const list<kvChecks> & checks = checkCreator_.getChecks();
-  if ( checks.empty() )
-    return logEnd_( "No appropriate checks found" );
+	//  relevant checks (QC1) for this observation
+	const list<kvChecks> & checks = checkCreator_.getChecks();
+	if (checks.empty())
+		return logEnd_("No appropriate checks found");
 
-  meteod.resetFlags(stinfo); // what is this? And why does things fail when I move this line?
+	meteod.resetFlags(stinfo); // what is this? And why does things fail when I move this line?
 
-  // Loop through checks
-  for ( list<kvChecks>::const_iterator cp = checks.begin(); cp != checks.end(); ++ cp ) {
-    try {
-      CheckCreator::ScriptList scripts;
-      checkCreator_.getScripts( scripts, * cp );
-      for ( CheckCreator::ScriptList::const_iterator script = scripts.begin(); script != scripts.end(); ++ script )
-	runCheck( * script, * cp );
-    }
-    catch ( kvQABaseMeteodata::SkipCheck & ) {
-      log_( "CheckRunner::runCheck skipping check (obs_program)" );
-    }
-    catch ( std::exception & e ) {
-      log_( e.what(), Error );
-    }
-  }
+	// Loop through checks
+	for (list<kvChecks>::const_iterator cp = checks.begin(); cp != checks.end(); ++cp)
+	{
+		try
+		{
+			CheckCreator::ScriptList scripts;
+			checkCreator_.getScripts(scripts, *cp);
+			for (CheckCreator::ScriptList::const_iterator script =
+					scripts.begin(); script != scripts.end(); ++script)
+				runCheck(*script, *cp);
+		} catch (kvQABaseMeteodata::SkipCheck &)
+		{
+			log_("CheckRunner::runCheck skipping check (obs_program)");
+		} catch (std::exception & e)
+		{
+			log_(e.what(), Error);
+		}
+	}
 
-
-  logEnd_( "Done processing", Debug );
-  LOGINFO( "CheckRunner::runChecks FINISHED" << endl );
+	logEnd_("Done processing", Debug);
+	LOGINFO( "CheckRunner::runChecks FINISHED" << endl );
 }
-
 
 namespace
 {
-  path logPath( const kvalobs::kvStationInfo & stinfo, const path & start_logpath )
-  {
-    path log_dir(start_logpath);
-    const path stationDirectory = boost::lexical_cast<string>(stinfo.stationID());
+path logPath(const kvalobs::kvStationInfo & stinfo, const path & start_logpath)
+{
+	path log_dir(start_logpath);
+	const path stationDirectory = boost::lexical_cast<string>(
+			stinfo.stationID());
 
-    const std::string obsTime = stinfo.obstime().isoDate();
-    log_dir /= stationDirectory/obsTime;
+	const std::string obsTime = stinfo.obstime().isoDate();
+	log_dir /= stationDirectory / obsTime;
 
-    boost::filesystem::create_directories( log_dir );
-    return log_dir;
-  }
+	boost::filesystem::create_directories(log_dir);
+	return log_dir;
+}
 
-  path logfilename( const std::string & clock, int version )
-  {
-      ostringstream filename;
-      filename << "log-" << clock;
-      if ( version ) filename << '_' << version;
-      filename << ".html";
-      return filename.str();
-  }
+path logfilename(const std::string & clock, int version)
+{
+	ostringstream filename;
+	filename << "log-" << clock;
+	if (version)
+		filename << '_' << version;
+	filename << ".html";
+	return filename.str();
+}
 
-  path getLogfilePath( const kvalobs::kvStationInfo & stinfo, const path & start_logpath )
-  {
-    namespace fs = boost::filesystem;
+path getLogfilePath(const kvalobs::kvStationInfo & stinfo,
+		const path & start_logpath)
+{
+	namespace fs = boost::filesystem;
 
-    fs::path log_dir = logPath( stinfo, start_logpath );
+	fs::path log_dir = logPath(stinfo, start_logpath);
 
-    std::string clock = stinfo.obstime().isoClock();
-    std::replace( clock.begin(), clock.end(), ':', '-' );
+	std::string clock = stinfo.obstime().isoClock();
+	std::replace(clock.begin(), clock.end(), ':', '-');
 
-    fs::path log_file;
-    for ( int i = 8; i >= 0; -- i ) {
-      log_file = log_dir/logfilename( clock, i );
-      if ( fs::exists( log_file ) ) {
-	fs::path rename_to = log_dir/logfilename( clock, i +1 );
-	fs::remove( rename_to );
-	fs::rename( log_file, rename_to );
-      }
-    }
-    return log_file.native_directory_string();
-  }
+	fs::path log_file;
+	for (int i = 8; i >= 0; --i)
+	{
+		log_file = log_dir / logfilename(clock, i);
+		if (fs::exists(log_file))
+		{
+			fs::path rename_to = log_dir / logfilename(clock, i + 1);
+			fs::remove(rename_to);
+			fs::rename(log_file, rename_to);
+		}
+	}
+	return log_file.native_directory_string();
+}
 }
 
 HtmlStream * CheckRunner::openHTMLStream()
 {
-  HtmlStream * html = 0;
-  try
-  {
-    html = new HtmlStream;
+	HtmlStream * html = 0;
+	try
+	{
+		html = new HtmlStream;
 
-    path logfile = getLogfilePath( stinfo, logpath_ );
+		path logfile = getLogfilePath(stinfo, logpath_);
 
-    LOGINFO( "CheckRunner::runChecks for station:" << stinfo.stationID()
-        << " and obstime:" << stinfo.obstime() << endl
-        << "Logging all activity to:" << logfile.native_file_string() << endl );
+		LOGINFO( "CheckRunner::runChecks for station:" << stinfo.stationID()
+				<< " and obstime:" << stinfo.obstime() << endl
+				<< "Logging all activity to:" << logfile.native_file_string() << endl );
 
-    if ( !html->open( logfile.native_file_string() ) )
-      throw std::runtime_error( "Failed to create logfile for the html output. Filename:\n" + logfile.native_file_string() );
+		if (!html->open(logfile.native_file_string()))
+			throw std::runtime_error(
+					"Failed to create logfile for the html output. Filename:\n"
+							+ logfile.native_file_string());
 
-    Logger::createLogger( "html", html );
-    Logger::logger("html").logLevel( DEBUG );
+		Logger::createLogger("html", html);
+		Logger::logger("html").logLevel(DEBUG);
 
-    IDLOGINFO( "html", "<h1>"
-        << "CheckRunner::runChecks for station:" << stinfo.stationID()
-            << " and obstime:" << stinfo.obstime()
-            << "</h1>" << endl );
-    IDLOGINFO( "html", "<CODE><PRE>" );
-  }
-  catch ( std::exception & e )
-  {
-    LOGERROR( "Error when creating logfile: " << e.what() << "\nUsing /dev/null for logging!");
-    html->open( "/dev/null" );
-  }
-  return html;
+		IDLOGINFO( "html", "<h1>"
+				<< "CheckRunner::runChecks for station:" << stinfo.stationID()
+				<< " and obstime:" << stinfo.obstime()
+				<< "</h1>" << endl );
+		IDLOGINFO( "html", "<CODE><PRE>" );
+	} catch (std::exception & e)
+	{
+		LOGERROR( "Error when creating logfile: " << e.what() << "\nUsing /dev/null for logging!");
+		html->open("/dev/null");
+	}
+	return html;
 }
--- src/kvQabased/checkrunner.h
+++ src/kvQabased/checkrunner.h
@@ -1,33 +1,33 @@
 /*
-  Kvalobs - Free Quality Control Software for Meteorological Observations 
+ Kvalobs - Free Quality Control Software for Meteorological Observations
 
-  $Id: checkrunner.h,v 1.1.2.5 2007/09/27 09:02:21 paule Exp $                                                       
+ $Id: checkrunner.h,v 1.1.2.5 2007/09/27 09:02:21 paule Exp $
 
-  Copyright (C) 2007 met.no
+ Copyright (C) 2007 met.no
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: kvalobs-dev@met.no
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: kvalobs-dev@met.no
 
-  This file is part of KVALOBS
+ This file is part of KVALOBS
 
-  KVALOBS is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as 
-  published by the Free Software Foundation; either version 2 
-  of the License, or (at your option) any later version.
-  
-  KVALOBS is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License along 
-  with KVALOBS; if not, write to the Free Software Foundation Inc., 
-  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ KVALOBS is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ KVALOBS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with KVALOBS; if not, write to the Free Software Foundation Inc.,
+ 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #ifndef _checkrunner_h
 #define _checkrunner_h
 
@@ -42,85 +42,84 @@
 #include <kvalobs/kvChecks.h>
 #include <boost/filesystem/path.hpp>
 
-
 /*
-TODO:
-  add a way to insert replacement (model) values after all checks have run.
-  - How to:
-    - Call a function after all checks have been run.
-      - Loop trough all variables from check
-        - if missing original value:
-          - insert value, after some criteria (model value)
-      - Implementation tightly coupled with QABase.
-    - Use a perl script, fetched from database
-      - Make sure the check is run last.
-      - Loosely coupled missing substitution.
-      - Easy for anyone to write a new algorithm
-      - Possible problem: The check will need to know about corrected value (if it is missing)
-      - Solved by giving fmis to the check.(?)
-*/
-
+ TODO:
+ add a way to insert replacement (model) values after all checks have run.
+ - How to:
+ - Call a function after all checks have been run.
+ - Loop trough all variables from check
+ - if missing original value:
+ - insert value, after some criteria (model value)
+ - Implementation tightly coupled with QABase.
+ - Use a perl script, fetched from database
+ - Make sure the check is run last.
+ - Loosely coupled missing substitution.
+ - Easy for anyone to write a new algorithm
+ - Possible problem: The check will need to know about corrected value (if it is missing)
+ - Solved by giving fmis to the check.(?)
+ */
 
 /**
  * Controls check running for a single message.
  */
 class CheckRunner
 {
-  public:
-    /**
-     * Constructor.
-     * 
-     * @param params The message to be controlled
-     * @param con A database connection to the kvalobs database.
-     * @param logpath path for logging.
-     */
-    CheckRunner( const kvalobs::kvStationInfo & params,
-	kvQABaseDBConnection & con,
-        const boost::filesystem::path & logpath );
-    
-    ~CheckRunner();
+public:
+	/**
+	 * Constructor.
+	 *
+	 * @param params The message to be controlled
+	 * @param con A database connection to the kvalobs database.
+	 * @param logpath path for logging.
+	 */
+			CheckRunner(const kvalobs::kvStationInfo & params,
+					kvQABaseDBConnection & con,
+					const boost::filesystem::path & logpath);
 
-    /**
-     * Set logpath (before first runchecks)
-     */
-    void logpath( const boost::filesystem::path & logp )
-    {
-      logpath_ = logp;
-    }
+	~CheckRunner();
 
-    /**
-     * Main routine. Runs all relevant checks for station
-     *
-     * @param forceCheck If true, run checks on data, even if it has been
-     * modified by HQC, or otherwise should not normally be checked.
-     *
-     * @throw std::exception of some sort on error.
-     */
-    void operator() ( bool forceCheck = false );
+	/**
+	 * Set logpath (before first runchecks)
+	 */
+	void logpath(const boost::filesystem::path & logp)
+	{
+		logpath_ = logp;
+	}
 
-  private:
-    bool shouldProcess();
-    bool hqcCorrected();
-    bool dataWasCheckedBefore();
-    void updateStaticVariables();
-    void findChecks( std::list<kvalobs::kvChecks> & out );
+	/**
+	 * Main routine. Runs all relevant checks for station
+	 *
+	 * @param forceCheck If true, run checks on data, even if it has been
+	 * modified by HQC, or otherwise should not normally be checked.
+	 *
+	 * @throw std::exception of some sort on error.
+	 */
+	void operator()(bool forceCheck = false);
 
-    void runCheck( const std::string & checkScript, const kvalobs::kvChecks & check );
+private:
+	bool shouldProcess();
+	bool hqcCorrected();
+	bool dataWasCheckedBefore();
+	void updateStaticVariables();
+	void findChecks(std::list<kvalobs::kvChecks> & out);
 
-    CheckCreator checkCreator_;
+	void runCheck(const std::string & checkScript,
+			const kvalobs::kvChecks & check);
 
-    const kvalobs::kvStationInfo & stinfo;
-    kvQABaseDBConnection dbcon;
+	CheckCreator checkCreator_;
 
-    kvObsPgmList oprogramlist;
-    kvQABaseMeteodata meteod; // Meteorological data manager
+	const kvalobs::kvStationInfo & stinfo;
+	kvQABaseDBConnection dbcon;
 
-    /// Path to logfile(s)
-    boost::filesystem::path logpath_;
-    /// The next time the static tables should be updated.
-    miutil::miTime updateStaticTime;
+	kvObsPgmList oprogramlist;
+	kvQABaseMeteodata meteod; // Meteorological data manager
 
-    milog::HtmlStream * openHTMLStream();
+	/// Path to logfile(s)
+	boost::filesystem::path logpath_;
+	/// The next time the static tables should be updated.
+	miutil::miTime updateStaticTime;
+
+	milog::HtmlStream * openHTMLStream();
 };
 
 #endif
