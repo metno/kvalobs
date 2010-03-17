9826372a47be3f8b4fd50023f240401bbf825330
Børge Moe <borge.moe@met.no>
2010-03-17 11:14:07 +0000

    Complete the split out of kvsynopd.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1279 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/script/dbadmin/generated_synop.sql
+++
@@ -1,9 +0,0 @@
---
--- Select for å finne alle stasjoner som vi genererer SYNOP for
--- og som skal legges inn i tabellen generated_types og typeid blir satt 
--- til -1*typeid.
---
-select stationid from data where obstime>(timestamp 'today'-interval'7 day') 
-       AND ((typeid>=300 AND typeid<400 and typeid!=302) OR  
-	typeid=3 OR typeid=6) 	
-	GROUP BY stationid order by stationid;
--- src/script/dbadmin/kvsynopcpy.sql
+++
@@ -1,9 +0,0 @@
--- 
--- This sql script is used to make a copy of a kvsynopd database.
--- 
-
-.mode insert data
-select * from data where obstime>=date('now', '-7  day');
-
-.mode insert synop
-select * from synop where obstime>=date('now', '-31  day');
