0e78220fd6e5fb6a8f773de0591d66c22a385cfb
Vegard Bønes <vegard.bones@met.no>
2010-03-29 08:00:45 +0000

    moved some lines to make upgrade run faster
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@1361 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvalobs_database/upgrade/2.1.1.sql
+++ src/kvalobs_database/upgrade/2.1.1.sql
@@ -1,5 +1,9 @@
 BEGIN;
 
+TRUNCATE data_history;
+TRUNCATE text_data_history;
+
+
 CREATE INDEX data_history_main_index ON data_history (stationid, obstime, paramid, level, sensor, typeid);
 CREATE INDEX data_history_obstime_index ON data_history (obstime);
 CREATE INDEX data_history_tbtime_index ON data_history (tbtime);
@@ -10,7 +14,6 @@ DROP TRIGGER backup_text_data ON text_data;
 CREATE TRIGGER backup_text_data AFTER INSERT OR UPDATE ON text_data FOR EACH ROW EXECUTE PROCEDURE backup_old_text_data();
 
 ALTER TABLE data_history ADD COLUMN modificationtime timestamp NOT NULL DEFAULT timezone('UTC', now());
-TRUNCATE data_history;
 CREATE INDEX data_history_modificationtime_index ON data_history (modificationtime);
 
 CREATE OR REPLACE FUNCTION 
@@ -45,7 +48,6 @@ LANGUAGE 'plpgsql';
 
 
 ALTER TABLE text_data_history ADD COLUMN modificationtime timestamp NOT NULL DEFAULT timezone('UTC', now());
-TRUNCATE text_data_history;
 CREATE INDEX text_data_history_modificationtime_index ON text_data_history (modificationtime);
 CREATE INDEX text_data_history_main_index ON text_data_history (stationid, obstime, paramid, typeid);
 
