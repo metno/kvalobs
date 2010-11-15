dd1f74a9a27419fbc0b045296b02ad182fe7eb1d
Vegard Bønes <vegard.bones@met.no>
2010-11-15 13:21:06 +0000

    modified log cleaning to adapt to new log file system
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@2360 422d51e2-5941-41ac-8e65-d5b5931239be

--- debian_files/scripts/qabase_cron.sh
+++ debian_files/scripts/qabase_cron.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 
 ##QABASELOGDIR=/metno/kvalobs/var/log/html
-QABASELOGDIR=$(kvconfig --localstatedir)/log/kvalobs/html/
+QABASELOGDIR=$(kvconfig --localstatedir)/log/kvalobs/checks
 
-#find $QABASELOGDIR -name '*.html' -type f -mtime +0 -exec rm -f {} \;
-find $QABASELOGDIR -xdev -name '*.html' -type f -mtime +3 -exec rm -f {} \;
-#find $QABASELOGDIR -name '*.html' -type f  -exec rm -f {} \;
-find $QABASELOGDIR -xdev -type d -ctime +1 -exec rmdir --ignore-fail-on-non-empty {} \;
+find $QABASELOGDIR -xdev -type d -ctime +7 -empty -exec rmdir --ignore-fail-on-non-empty {} \;
+find $QABASELOGDIR -xdev -name '*.log.gz' -type f -mtime +7 -exec rm -f {} \;
+find $QABASELOGDIR -xdev -name '*.log' -type f -mtime +0 -exec gzip {} \;
 
 # create nice map of existing log-files (output: html)
 ####find $QABASELOGDIR -name '*.html' -type f | /metno/kvalobs/bin/log_map > $QABASELOGDIR/log_map.html
