c50ffea318e299456bbe1139393875ea56381c27
Børge Moe <borge.moe@met.no>
2011-01-25 08:30:37 +0000

    Update set fmis and fpre as of the last suggestion.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@2619 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/lib/decoder/decoderbase/DataUpdateTransaction.cc
+++ src/lib/decoder/decoderbase/DataUpdateTransaction.cc
@@ -756,17 +756,11 @@ replaceData( dnmi::db::Connection *conection,
    for( list<kvalobs::kvData>::iterator it = oldData.begin();
         it != oldData.end(); ++it ) {
       it->corrected( -32767 );
+
       cinfo = it->controlinfo();
-      fmis = cinfo.MissingFlag();
-
-      if( fmis == 1 || fmis == 3 )
-         continue;
-
-      if( fmis == 0 ) {
-        cinfo.MissingFlag( 2 );
-      }
-
       cinfo.setControlFlag( kvQCFlagTypes::f_fpre, 7 );
+      cinfo.setControlFlag( kvQCFlagTypes::f_fmis, 2 );
+
       it->controlinfo( cinfo );
    }
 
