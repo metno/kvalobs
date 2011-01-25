a52c40b7c29656c7a8a7ef71ba62b419f2ecd158
Børge Moe <borge.moe@met.no>
2011-01-25 11:21:39 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@2628 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/norcom2kv/testWMORaport.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-FILELIST="data0000 data0600 data0300 data0900"
-
-for FILE in $FILELIST ; do
-  ./testWMORaport synop/$FILE > $FILE.pre ;
-  echo "diff -c -b -B regres/$FILE.reg $FILE.pre";
-  diff -c -b -B regres/$FILE.reg $FILE.pre
-done
