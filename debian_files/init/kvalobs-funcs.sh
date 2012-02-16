6d3de4b638a9cbaea9d5b5205f0c8b8229fbaf1c
Børge Moe <borge.moe@met.no>
2012-02-16 15:42:15 +0000

    Make the init script useful on a test machine also.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@3301 422d51e2-5941-41ac-8e65-d5b5931239be

--- debian_files/init/kvalobs-funcs.sh
+++ debian_files/init/kvalobs-funcs.sh
@@ -1,5 +1,13 @@
 primary_host="kvalobs1 kvalobs2"
-ipalias=/home/borgem/alias_kvalobsnew
+ipalias="/usr/local/sbin/alias_kvalobs"
+
+if [ ! -x "$ipalias" ]; then
+	ipalias="/home/borgem/alias_kvalobsnew"
+fi
+
+#if [ "$TEST" = "true" ]; then
+#	ipalias=true
+#fi
 
 #logfile=/var/log/kvalobs/kvalobs-init.log
 
@@ -7,13 +15,13 @@ if [ -f "/etc/kvalobs/kvalobs-init.conf" ]; then
 	. /etc/kvalobs/kvalobs-init.conf
 fi
 
-if [ -n "$PRIMARY_HOST" ]; then
-	primary_host="$PRIMARY_HOST"
-fi
+#if [ -n "$PRIMARY_HOST" ]; then
+#	primary_host="$PRIMARY_HOST"
+#fi
 
-if [ -n "$KVALOBS_IPALIAS" ]; then
-	ipalias="$KVALOBS_IPALIAS"
-fi
+#if [ -n "$KVALOBS_IPALIAS" ]; then
+#	ipalias="$KVALOBS_IPALIAS"
+#fi
 
 log()
 {
@@ -30,36 +38,36 @@ has_ip_alias()
     local res=0
 
     for host in $primary_host; do 
-	if [ "$me" = "$host" ];then
-	    is_me=$host
-	fi
+		if [ "$me" = "$host" ];then
+	    	is_me=$host
+		fi
     done
     
     if [ -z "$is_me" ]; then
-	echo "undef"
-	return 0
+		echo "undef"
+		return 0
     fi
 
     if [ ! -x "$ipalias" ]; then
-	log "Cant find ipalias '$ipalias'"
-	return 1
+		log "Cant find ipalias '$ipalias'"
+		return 1
     fi
 
     if [ "x$logfile" = "x" ]; then
-	$ipalias start > /dev/null 2>&1 || res=$? 
+		$ipalias start > /dev/null 2>&1 || res=$? 
     else
-	$ipalias start >> $logfile 2>&1 || res=$?  
+		$ipalias start >> $logfile 2>&1 || res=$?  
     fi
 
     if [ "$res" = "2" -o "$res" = "0" ]; then
-	log "This machine has the kvalobs ip alias." 
-	echo "true"
+		log "This machine has the kvalobs ip alias." 
+		echo "true"
     elif [ "$res" = "1" ]; then
-	log "Another machine has the kvalobs ip alias."
-	echo "false"
+		log "Another machine has the kvalobs ip alias."
+		echo "false"
     else
-	log "FAILED - kvalobs ip alias."
-	return 1
+		log "FAILED - kvalobs ip alias."
+		return 1
     fi
 }
 
@@ -69,22 +77,22 @@ stop_ip_alias()
     local mylog
 
     if [ -z "$logfile" ]; then
-	mylog="/dev/null"
+		mylog="/dev/null"
     else
-	mylog="$logfile"
+		mylog="$logfile"
     fi
    
     if [ ! -x "$ipalias" ]; then
-	echo "Cant find ipalias '$ipalias'" >> $mylog
-	return 1
+		echo "Cant find ipalias '$ipalias'" >> $mylog
+		return 1
     fi
 
     $ipalias stop >> $mylog 2>&1 || res=$? 
     
     case "$res" in
-	0) echo "kvalobs - IP alias deleted on this machine." >> $mylog ;;
-	1) echo "kvalobs - IP alias not running on this machine." >> $mylog ;;
-	*) echo "kvalobs - IP alias ($res)" >> $mylog 
+		0) echo "kvalobs - IP alias deleted on this machine." >> $mylog ;;
+		1) echo "kvalobs - IP alias not running on this machine." >> $mylog ;;
+		*) echo "kvalobs - IP alias ($res)" >> $mylog 
            return 1 ;;
     esac
 }
@@ -98,9 +106,9 @@ start_as_master()
 
 
     if [ -z "$logfile" ]; then
-	mylog="/dev/null"
+		mylog="/dev/null"
     else
-	mylog="$logfile"
+		mylog="$logfile"
     fi
     
     echo "USER: $USER" >>  $mylog
@@ -110,19 +118,19 @@ start_as_master()
     pgrepctl-kvalobs --start-as-master >> $mylog 2>&1 || res=$?
 
     if [ "$res" = "0" ]; then
-	echo "kvalobs - Started postgresql on this server as master."  | tee -a $mylog
+		echo "kvalobs - Started postgresql on this server as master."  | tee -a $mylog
     else
-	echo "kvalobs - Failed starting postgresql on this server as master." | tee -a $mylog
-	return 1
+		echo "kvalobs - Failed starting postgresql on this server as master." | tee -a $mylog
+		return 1
     fi
 
     kvstart || res=$?
     
     if [ "$res" = "0" ]; then
-	echo "kvalobs - started."  | tee -a $mylog
+		echo "kvalobs - started."  | tee -a $mylog
     else
-	echo "kvalobs - Failed to start kvalobs." | tee -a $mylog
-	return 1
+		echo "kvalobs - Failed to start kvalobs." | tee -a $mylog
+		return 1
     fi
 }
 
@@ -133,18 +141,18 @@ start_as_slave()
     local res=0
 
     if [ -z "$logfile" ]; then
-	mylog="/dev/null"
+		mylog="/dev/null"
     else
-	mylog=$logfile
+		mylog=$logfile
     fi
     
     pgrepctl-kvalobs --start-as-slave >> $mylog 2>&1 || res=$?
 
     if [ "$res" = "0" ]; then
-	echo "kvalobs - Started postgresql on this server as an slave." | tee -a $mylog
+		echo "kvalobs - Started postgresql on this server as an slave." | tee -a $mylog
     else
-	echo "kvalobs - Failed starting postgresql on this server as an slave." | tee -a $mylog
-	return 1
+		echo "kvalobs - Failed starting postgresql on this server as an slave." | tee -a $mylog
+		return 1
     fi
 }
 
@@ -155,15 +163,15 @@ stop_kvalobs()
     local cmd
 
     if [ -z "$logfile" ]; then
-	mylog="/dev/null"
+		mylog="/dev/null"
     else
-	mylog=$logfile
+		mylog=$logfile
     fi
 
     if [ $# -le 1 ]; then
-	cmd="cluster"
+		cmd="cluster"
     else
-	cmd="$1"
+		cmd="$1"
     fi
     
     kvstop || res=$?
@@ -171,14 +179,14 @@ stop_kvalobs()
     res=0
 
     case "$1" in
-	slave) pgrepctl-kvalobs --stop >> $mylog 2>&1 || res=$? ;;
-	cluster) pgrepctl-kvalobs --stop-cluster >> $mylog 2>&1 || res=$? ;;
-	*) return 1 ;;
+		slave) pgrepctl-kvalobs --stop >> $mylog 2>&1 || res=$? ;;
+		cluster) pgrepctl-kvalobs --stop-cluster >> $mylog 2>&1 || res=$? ;;
+		*) return 1 ;;
     esac
 
     if [ "$res" = "0" ]; then
-	echo "kvalobs - Stopped kvalobs. ($cmd)" | tee -a $mylog
+		echo "kvalobs - Stopped kvalobs. ($cmd)" | tee -a $mylog
     else
-	echo "kvalobs - Failed to stop kvalobs. ($cmd)" | tee -a $mylog
+		echo "kvalobs - Failed to stop kvalobs. ($cmd)" | tee -a $mylog
     fi
 }
