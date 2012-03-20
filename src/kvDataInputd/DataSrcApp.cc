74c7705d0c0c9767efc08da9757e44198eb2389e
Børge Moe <borge.moe@met.no>
2012-03-20 14:01:09 +0000

    Show the correct decoder versions that is loaded at startup.
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@3433 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvDataInputd/DataSrcApp.cc
+++ src/kvDataInputd/DataSrcApp.cc
@@ -18,16 +18,16 @@
   modify it under the terms of the GNU General Public License as 
   published by the Free Software Foundation; either version 2 
   of the License, or (at your option) any later version.
-  
+
   KVALOBS is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License along 
   with KVALOBS; if not, write to the Free Software Foundation Inc., 
   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ */
 #include <signal.h> 
 #include <boost/lexical_cast.hpp>
 #include <milog/milog.h>
@@ -45,58 +45,58 @@ using namespace boost;
 volatile sig_atomic_t sigTerm=0;
 
 DataSrcApp::DataSrcApp(int argn, char **argv,
-		       int               nConnections_,
-		       const char *opt[][2])
-  :KvApp(argn, argv, opt), refMgr(CKvalObs::CManager::ManagerInput::_nil()),
-   ok(false), shutdown_(false)
+                       int               nConnections_,
+                       const char *opt[][2])
+:KvApp(argn, argv, opt), refMgr(CKvalObs::CManager::ManagerInput::_nil()),
+ ok(false), shutdown_(false)
 {
-  miutil::conf::ConfSection *conf;
-  string logdir( kvPath("logdir") );
-  string myPath=kvPath("pkglibdir");
-  myPath+="/decode";
-  
-  conf=KvApp::getConfiguration();
-  
-  if(!conf){
-  		LOGFATAL("Cant read configuration file: " << getConfFile() << endl);
-  		cerr << "Cant read configuration file: " << getConfFile() << endl;
-  		exit(1);
-  }
-  
-  miutil::conf::ValElementList val=conf->getValue("database.dbdriver");
+   miutil::conf::ConfSection *conf;
+   string logdir( kvPath("logdir") );
+   string myPath=kvPath("pkglibdir");
+   myPath+="/decode";
 
-  if(val.size()==1)
-     dbDriver=val[0].valAsString();
-  
-  connectStr=KvApp::createConnectString();
-   
-  if(dbDriver.empty())
-    dbDriver="pgdriver.so";
+   conf=KvApp::getConfiguration();
 
-  decoderMgr.setDecoderPath(myPath);
+   if(!conf){
+      LOGFATAL("Cant read configuration file: " << getConfFile() << endl);
+      cerr << "Cant read configuration file: " << getConfFile() << endl;
+      exit(1);
+   }
 
-  nConnections=registerDb(nConnections_);
- 
-  if(nConnections<1)
-    return;
+   miutil::conf::ValElementList val=conf->getValue("database.dbdriver");
 
-  if(!registerAllDecoders())
-    return;
+   if(val.size()==1)
+      dbDriver=val[0].valAsString();
 
-  if(!registerParams())
-    return;
+   connectStr=KvApp::createConnectString();
 
-  if(!registerTypes())
-    return;
+   if(dbDriver.empty())
+      dbDriver="pgdriver.so";
 
-  milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "failed", milog::DEBUG );
-  milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "duplicates", milog::DEBUG );
-  milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "updated", milog::DEBUG );
-  milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "retry", milog::DEBUG );
-  milog::createGlobalLogger( logdir, "kvDataInputd", "transaction", milog::DEBUG,
-                             200, 1,  new milog::StdLayout1() );
+   decoderMgr.setDecoderPath(myPath);
 
-  ok=true;
+   nConnections=registerDb(nConnections_);
+
+   if(nConnections<1)
+      return;
+
+   if(!registerAllDecoders())
+      return;
+
+   if(!registerParams())
+      return;
+
+   if(!registerTypes())
+      return;
+
+   milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "failed", milog::DEBUG );
+   milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "duplicates", milog::DEBUG );
+   milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "updated", milog::DEBUG );
+   milog::createGlobalLogger( logdir, "kvDataInputd_transaction", "retry", milog::DEBUG );
+   milog::createGlobalLogger( logdir, "kvDataInputd", "transaction", milog::DEBUG,
+                              200, 1,  new milog::StdLayout1() );
+
+   ok=true;
 }
 
 /*
@@ -106,115 +106,115 @@ DataSrcApp::DataSrcApp(int argn, char **argv,
 CKvalObs::CManager::ManagerInput_ptr 
 DataSrcApp::lookUpManager(bool forceNS, bool &usedNS)
 {
-  CORBA::Object_var obj;
-  CKvalObs::CManager::ManagerInput_ptr ptr;
+   CORBA::Object_var obj;
+   CKvalObs::CManager::ManagerInput_ptr ptr;
 
-  usedNS=false;
+   usedNS=false;
 
-  while(true){
-    if(forceNS){
-      usedNS=true;
-      
-      obj=getRefInNS("kvManagerInput");
-      
-      if(CORBA::is_nil(obj))
-	throw LookUpException("EXCEPTION: Can't obtain a reference for 'kvManagerInput'\n           from the CORBA nameserver!");
-      
-      ptr=CKvalObs::CManager::ManagerInput::_narrow(obj);
-      
-      if(CORBA::is_nil(ptr))
-	throw LookUpException("EXCEPTION: Can't narrow reference for 'kvManagerInput'!");
-      
-      refMgr=ptr;
-      
-      return ptr;
-    }
+   while(true){
+      if(forceNS){
+         usedNS=true;
 
-    if(CORBA::is_nil(refMgr))
-      forceNS=true;
-    else
-      return refMgr;
-  }
+         obj=getRefInNS("kvManagerInput");
+
+         if(CORBA::is_nil(obj))
+            throw LookUpException("EXCEPTION: Can't obtain a reference for 'kvManagerInput'\n           from the CORBA nameserver!");
+
+         ptr=CKvalObs::CManager::ManagerInput::_narrow(obj);
+
+         if(CORBA::is_nil(ptr))
+            throw LookUpException("EXCEPTION: Can't narrow reference for 'kvManagerInput'!");
+
+         refMgr=ptr;
+
+         return ptr;
+      }
+
+      if(CORBA::is_nil(refMgr))
+         forceNS=true;
+      else
+         return refMgr;
+   }
 }
 
 bool 
 DataSrcApp::sendInfoToManager(const kvalobs::kvStationInfoList &info_)
 {
-  bool forceNS=false;
-  bool usedNS=false;
-  CORBA::Long  infoIndex;
-  CORBA::Long  paramIndex;
-  kvalobs::kvStationInfoList &info=const_cast<kvalobs::kvStationInfoList&>(info_);
-  
-  CKvalObs::CManager::ManagerInput_ptr mngr;
-  CKvalObs::StationInfoList infoList;
-  kvalobs::IkvStationInfoList it;
+   bool forceNS=false;
+   bool usedNS=false;
+   CORBA::Long  infoIndex;
+   CORBA::Long  paramIndex;
+   kvalobs::kvStationInfoList &info=const_cast<kvalobs::kvStationInfoList&>(info_);
 
-  infoList.length(info.size());
+   CKvalObs::CManager::ManagerInput_ptr mngr;
+   CKvalObs::StationInfoList infoList;
+   kvalobs::IkvStationInfoList it;
 
-  LOGDEBUG("sendInfoToManager: \n" <<
-	   "info::size=" << info.size() << "\n" <<
-	   "infoList::length=" << infoList.length() << "\n");
+   infoList.length(info.size());
 
-  it=info.begin();
+   LOGDEBUG("sendInfoToManager: \n" <<
+            "info::size=" << info.size() << "\n" <<
+            "infoList::length=" << infoList.length() << "\n");
 
-  if(it==info.end()){
-    LOGDEBUG("sendInfoToManager: \n"<<
-	     "info_::size=" << info_.size() << "\n" <<
-	     "No data to send to kvManagerd!\n");
-    return true;
-  }
+   it=info.begin();
 
-  for(infoIndex=0; it!=info.end(); it++, infoIndex++){
-    infoList[infoIndex].stationId=it->stationID();
-    infoList[infoIndex].obstime=it->obstime().isoTime().c_str();
-    infoList[infoIndex].typeId_=it->typeID();
-  }
+   if(it==info.end()){
+      LOGDEBUG("sendInfoToManager: \n"<<
+               "info_::size=" << info_.size() << "\n" <<
+               "No data to send to kvManagerd!\n");
+      return true;
+   }
 
-  LOGDEBUG("sendInfoToManager: StationInfo packed!\n");
+   for(infoIndex=0; it!=info.end(); it++, infoIndex++){
+      infoList[infoIndex].stationId=it->stationID();
+      infoList[infoIndex].obstime=it->obstime().isoTime().c_str();
+      infoList[infoIndex].typeId_=it->typeID();
+   }
+
+   LOGDEBUG("sendInfoToManager: StationInfo packed!\n");
 
 
-  try{
-    for(int i=0; i<2; i++){
-      mngr=lookUpManager(forceNS, usedNS);
+   try{
+      for(int i=0; i<2; i++){
+         mngr=lookUpManager(forceNS, usedNS);
 
-      try{
-	mngr->newData(infoList);
-	return true;
+         try{
+            mngr->newData(infoList);
+            return true;
+         }
+         catch(CORBA::TRANSIENT &ex){
+            LOGINFO("(kvmanager) Exception CORBA::TRANSIENT!\n");
+         }
+         catch(CORBA::COMM_FAILURE &ex){
+            LOGERROR("(kvmanager) Exception CORBA::COMM_FAILURE!\n" <<
+                     "Is kvManager running\n");
+         }
+         catch(...){
+            LOGERROR("(kvmanager) Exception unknown!\n");
+            return false;
+         }
+
+         if(usedNS){
+            LOGWARN("can't send data to kvManagerInput!\n");
+            return false;
+         }
+         forceNS=true;
+
       }
-      catch(CORBA::TRANSIENT &ex){
-	LOGINFO("(kvmanager) Exception CORBA::TRANSIENT!\n");
-      }
-      catch(CORBA::COMM_FAILURE &ex){
-	LOGERROR("(kvmanager) Exception CORBA::COMM_FAILURE!\n" <<
-		"Is kvManager running\n");
-      }
-      catch(...){
-	LOGERROR("(kvmanager) Exception unknown!\n");
-	return false;
-      }
+   }
+   catch(LookUpException &ex){
+      LOGWARN("LookUpException: " << ex.what() << endl);
+      return false;
+   }
+   catch(...){
+      LOGWARN("Unknown Exception: endInfoToManager: \n" <<
+              "hmmm, very strange, a unkown exception!\n");
+      return false;
+   }
 
-      if(usedNS){
-	LOGWARN("can't send data to kvManagerInput!\n");
-	return false;
-      }
-      forceNS=true;
+   //Shall newer happend!
+   return false;
 
-    }
-  }
-  catch(LookUpException &ex){
-    LOGWARN("LookUpException: " << ex.what() << endl);
-    return false;
-  }
-  catch(...){
-    LOGWARN("Unknown Exception: endInfoToManager: \n" <<
-	    "hmmm, very strange, a unkown exception!\n");
-    return false;
-  }
-   
-  //Shall newer happend!
-  return false;
-  
 }
 
 
@@ -227,55 +227,55 @@ DataSrcApp::~DataSrcApp()
 int 
 DataSrcApp::registerDb(int nConn)
 {
-  Connection *con;
-  string driver( kvPath("pkglibdir")+"/db/"+dbDriver);
-  string drvId;
-  int    n=0;
-  
-  LOGINFO("registerDb: loading driver <" << driver << ">!\n");
+   Connection *con;
+   string driver( kvPath("pkglibdir")+"/db/"+dbDriver);
+   string drvId;
+   int    n=0;
 
-  if(!dbMgr.loadDriver(driver, drvId)){
-    //LOGFATAL("registerDb: Can't load driver <" << driver << ">\n");
-    LOGFATAL(dbMgr.getErr());
-    return 0;
-  }
-  
-  if( setAppNameForDb && !appName.empty() )
-     dbMgr.setAppName( appName );
+   LOGINFO("registerDb: loading driver <" << dbMgr.fixDriverName( driver ) << ">!\n");
 
-  LOGINFO("registerDb: Driver <" << drvId<< "> loaded!\n");
+   if(!dbMgr.loadDriver(driver, drvId)){
+      //LOGFATAL("registerDb: Can't load driver <" << driver << ">\n");
+      LOGFATAL(dbMgr.getErr());
+      return 0;
+   }
 
-  for(int i=0; i<nConn; i++){
-    con=0;
-    
-    while(!con && !inShutdown()){ 
-      con=dbMgr.connect(drvId, connectStr);
-      
-      if(!con){
-	LOGFATAL("registerDb: Can't create connection to <" 
-		 << drvId << ">\n ERROR message: " << dbMgr.getErr());
-	sleep(1);
-      }else{
-	conCache.addConnection(con);
-	n++;
+   if( setAppNameForDb && !appName.empty() )
+      dbMgr.setAppName( appName );
+
+   LOGINFO("registerDb: Driver <" << drvId<< "> loaded!\n");
+
+   for(int i=0; i<nConn; i++){
+      con=0;
+
+      while(!con && !inShutdown()){
+         con=dbMgr.connect(drvId, connectStr);
+
+         if(!con){
+            LOGFATAL("registerDb: Can't create connection to <"
+                  << drvId << ">\n ERROR message: " << dbMgr.getErr());
+            sleep(1);
+         }else{
+            conCache.addConnection(con);
+            n++;
+         }
+
       }
-      
-    }
-  }
+   }
 
 
-  return n;
+   return n;
 }
 
 bool 
 DataSrcApp::registerAllDecoders()
 {
-  if(decoderMgr.numberOfDecoders()<1){
-    LOGERROR("registerAllDecoders: Can't registers decoders!\n");
-    return false;
-  }
+   if(decoderMgr.numberOfDecoders()<1){
+      LOGERROR("registerAllDecoders: Can't registers decoders!\n");
+      return false;
+   }
 
-  return true;
+   return true;
 }
 
 
@@ -283,201 +283,201 @@ DataSrcApp::registerAllDecoders()
 bool
 DataSrcApp::registerParams()
 {
-  Result     *res=0;
-  Connection *con;
-  miString    msg;
-  miString    kode;   
-  int         id;
-  con=conCache.findFreeConnection();
+   Result     *res=0;
+   Connection *con;
+   miString    msg;
+   miString    kode;
+   int         id;
+   con=conCache.findFreeConnection();
 
-  if(!con){
-    LOGDEBUG("registerParams: SIGNAL: Stopped on signal!\n");
-    return false;
-  }
-  
-  try{
-    
-    res=con->execQuery("SELECT paramid,name FROM param");
+   if(!con){
+      LOGDEBUG("registerParams: SIGNAL: Stopped on signal!\n");
+      return false;
+   }
 
-    if(res && res->size()>0){
+   try{
 
-      while(res->hasNext()){
-	DRow row=res->next();
-	
-	try{
-	  id=lexical_cast<int>(row[0]);
-	  paramList.insert(Param(row[1], id)); 
-	}
-	catch(bad_lexical_cast &){
-	  LOGERROR("registerParams: BADNUM: kvnumber is not a number\n" <<
-	       "   kvnumber(" << row[0] << ")\n");
-	}
+      res=con->execQuery("SELECT paramid,name FROM param");
+
+      if(res && res->size()>0){
+
+         while(res->hasNext()){
+            DRow row=res->next();
+
+            try{
+               id=lexical_cast<int>(row[0]);
+               paramList.insert(Param(row[1], id));
+            }
+            catch(bad_lexical_cast &){
+               LOGERROR("registerParams: BADNUM: kvnumber is not a number\n" <<
+                        "   kvnumber(" << row[0] << ")\n");
+            }
+         }
+
+         delete res;
       }
-      
+   }
+   catch(SQLException &ex){
       delete res;
-    }
-  }
-  catch(SQLException &ex){
-    delete res;
-    conCache.freeConnection(con);
-    LOGERROR("registerParams: Exception: " << ex.what() << endl);
-    return false;
-  }
-  catch(...){
-    delete res;
-    conCache.freeConnection(con);
-    LOGERROR("registerParams: Exception: Unkown\n");
-  }
-    
-  conCache.freeConnection(con);
-  
-  //CERR(paramList);
+      conCache.freeConnection(con);
+      LOGERROR("registerParams: Exception: " << ex.what() << endl);
+      return false;
+   }
+   catch(...){
+      delete res;
+      conCache.freeConnection(con);
+      LOGERROR("registerParams: Exception: Unkown\n");
+   }
 
-  return true;
+   conCache.freeConnection(con);
+
+   //CERR(paramList);
+
+   return true;
 }
 
 bool
 DataSrcApp::registerTypes()
 {
-  Connection *con=conCache.findFreeConnection();
+   Connection *con=conCache.findFreeConnection();
 
-  if(!con){
-    LOGDEBUG("registerTypes: SIGNAL: Stopped on signal!\n");
-    return false;
-  }
+   if(!con){
+      LOGDEBUG("registerTypes: SIGNAL: Stopped on signal!\n");
+      return false;
+   }
 
-  kvalobs::kvDbGate gate(con);
- 
-  if(!gate.select(typeList)){
-    LOGFATAL("registerTypes failed: " << gate.getErrorStr());
-    conCache.freeConnection(con);
-    return false;
-  }
+   kvalobs::kvDbGate gate(con);
 
-  conCache.freeConnection(con);
-   
-  return true;
+   if(!gate.select(typeList)){
+      LOGFATAL("registerTypes failed: " << gate.getErrorStr());
+      conCache.freeConnection(con);
+      return false;
+   }
+
+   conCache.freeConnection(con);
+
+   return true;
 }
-  
+
 DecodeCommand*
 DataSrcApp::create(const char  *obsType_, 
-		   const char  *obs,
-		   long        timoutIn_msec,
-		   ErrCode     &errCode, 
-		   std::string &err)
+                   const char  *obs,
+                   long        timoutIn_msec,
+                   ErrCode     &errCode,
+                   std::string &err)
 {
-  DecoderBase   *dec;
-  DecodeCommand *decCmd;
-  Connection    *con;
-  miString      myErr;
+   DecoderBase   *dec;
+   DecodeCommand *decCmd;
+   Connection    *con;
+   miString      myErr;
 
-  //The call to conCache will block until a connection object
-  //is ready in the cache.
-  con=conCache.findFreeConnection();
+   //The call to conCache will block until a connection object
+   //is ready in the cache.
+   con=conCache.findFreeConnection();
 
-  if(!con){
-    errCode=NoDbConnection;
-    err="SIGNAL: Stopped on signal!";
-    return 0;
-  }
-  
-  //lookup the decoder based on obsType.
-  {
-    Lock lck(mutex);
-   
-    dec=decoderMgr.findDecoder(*con,
-			       paramList,
-			       typeList,
-			       obsType_,
-			       obs,
-			       myErr);
-  }
+   if(!con){
+      errCode=NoDbConnection;
+      err="SIGNAL: Stopped on signal!";
+      return 0;
+   }
 
-  if(!dec){
-    conCache.freeConnection(con);
-    errCode=NoDecoder;
-    err=myErr;
-    return 0;
-  }
-
-  try{
-    decCmd = new DecodeCommand(dec);
-  }
-  catch(...){
-    errCode=NoMem;
-    err="Out of memmory!";
-    
-    {
+   //lookup the decoder based on obsType.
+   {
       Lock lck(mutex);
-      decoderMgr.releaseDecoder(dec);
-    }
 
-    conCache.freeConnection(con);
-  }
-  
-  return decCmd;
- 
+      dec=decoderMgr.findDecoder(*con,
+                                 paramList,
+                                 typeList,
+                                 obsType_,
+                                 obs,
+                                 myErr);
+   }
+
+   if(!dec){
+      conCache.freeConnection(con);
+      errCode=NoDecoder;
+      err=myErr;
+      return 0;
+   }
+
+   try{
+      decCmd = new DecodeCommand(dec);
+   }
+   catch(...){
+      errCode=NoMem;
+      err="Out of memmory!";
+
+      {
+         Lock lck(mutex);
+         decoderMgr.releaseDecoder(dec);
+      }
+
+      conCache.freeConnection(con);
+   }
+
+   return decCmd;
+
 }
-  
+
 DecodeCommand*
 DataSrcApp::create(CORBA::Long report_id, 
-		   const char* report_type, 
-		   const char* obstime, 
-		   const char* data,
-		   long        timoutIn_msec,
-		   ErrCode     &errCode,
-		   std::string &err)
+                   const char* report_type,
+                   const char* obstime,
+                   const char* data,
+                   long        timoutIn_msec,
+                   ErrCode     &errCode,
+                   std::string &err)
 {
 }
 
 void 
 DataSrcApp::releaseDecodeCommand(DecodeCommand *command)
 {
-  DecoderBase   *dec;
+   DecoderBase   *dec;
 
-  if(!command){
-    LOGDEBUG("releaseDecodeCommand: ERROR command==0!");
-    return;
-  }
+   if(!command){
+      LOGDEBUG("releaseDecodeCommand: ERROR command==0!");
+      return;
+   }
 
-  dec=command->getDecoder();
+   dec=command->getDecoder();
 
-  if(!dec){
-    LOGDEBUG("releaseDecodeCommand: ERROR decoder==0!");
-    delete command;
-    return;
-  }
+   if(!dec){
+      LOGDEBUG("releaseDecodeCommand: ERROR decoder==0!");
+      delete command;
+      return;
+   }
 
-  conCache.freeConnection(dec->getConnection());
-  
-  {
-    Lock lck(mutex);
-    decoderMgr.releaseDecoder(dec);
-  }
-  
-  delete command;
+   conCache.freeConnection(dec->getConnection());
+
+   {
+      Lock lck(mutex);
+      decoderMgr.releaseDecoder(dec);
+   }
+
+   delete command;
 }
 
 bool                      
 DataSrcApp::put(dnmi::thread::CommandBase *command)
 {
-  try{
-    que.postAndBrodcast(command);
-  }
-  catch(...){
-    return false;
-  }
+   try{
+      que.postAndBrodcast(command);
+   }
+   catch(...){
+      return false;
+   }
 
-  return true;
+   return true;
 }
 
 
 dnmi::thread::CommandBase*                      
 DataSrcApp::remove(dnmi::thread::CommandBase *command)
 {
-  dnmi::thread::CommandBase *cmd;
-  
-  return que.remove(command);
+   dnmi::thread::CommandBase *cmd;
+
+   return que.remove(command);
 }
 
 
@@ -486,14 +486,14 @@ DataSrcApp::remove(dnmi::thread::CommandBase *command)
 bool                      
 DataSrcApp::isOk()const 
 { 
-  if(KvApp::isOk() && ok)
-    return true;
+   if(KvApp::isOk() && ok)
+      return true;
 
-  return false;
+   return false;
 }
 
 bool 
 DataSrcApp::inShutdown()const
 { 
-  return shutdown_ || sigTerm;
+   return shutdown_ || sigTerm;
 }
