22f02903bea4755999906592761809a85d69e394
Alexander Bürger <alexander.buerger@met.no>
2012-05-18 10:52:26 +0000

    added new tables for qc2 interpolation and statistical checks
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@3530 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/kvalobs_database/kvalobs_schema.sql
+++ src/kvalobs_database/kvalobs_schema.sql
@@ -588,3 +588,30 @@ REVOKE ALL ON ps_subscribers FROM public;
 GRANT ALL ON ps_subscribers TO kv_admin;
 GRANT SELECT ON ps_subscribers TO kv_read;
 GRANT SELECT, UPDATE, INSERT, DELETE ON ps_subscribers TO kv_write;
+
+
+CREATE TABLE qc2_statistical_reference_values (
+       stationid   INTEGER NOT NULL,
+       paramid     INTEGER NOT NULL,
+       day_of_year INTEGER NOT NULL,
+       key         TEXT NOT NULL,
+       value       FLOAT NOT NULL);
+
+REVOKE ALL ON qc2_statistical_reference_values FROM public;
+GRANT ALL ON qc2_statistical_reference_values TO kv_admin;
+GRANT SELECT ON qc2_statistical_reference_values TO kv_read;
+GRANT SELECT, UPDATE, INSERT, DELETE ON qc2_statistical_reference_values TO kv_write;
+
+CREATE TABLE qc2_interpolation_best_neighbors (
+       stationid        INTEGER NOT NULL,
+       neighborid       INTEGER NOT NULL,
+       paramid          INTEGER NOT NULL,
+       interpolation_id INTEGER NOT NULL,
+       fit_offset       FLOAT NOT NULL,
+       fit_slope        FLOAT NOT NULL,
+       fit_sigma        FLOAT NOT NULL);
+
+REVOKE ALL ON qc2_interpolation_best_neighbors FROM public;
+GRANT ALL ON qc2_interpolation_best_neighbors TO kv_admin;
+GRANT SELECT ON qc2_interpolation_best_neighbors TO kv_read;
+GRANT SELECT, UPDATE, INSERT, DELETE ON qc2_interpolation_best_neighbors TO kv_write;
