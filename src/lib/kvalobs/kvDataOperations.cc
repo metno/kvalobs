7190b8fe0a76c9d4c13a2f04ae0ef83a3904d48a
Alexander Bürger <alexander.buerger@met.no>
2012-07-16 13:25:16 +0000

    adapt hqc_distribute to flagsspec 9.11
     * not so nice: only data that were correctly marked as endpoint
       before running hqc_distribute (i.e. that had fd=4) will have
       the correct endpoint flag (fd=0xA) afterwards
    
    git-svn-id: https://svn.met.no/kvoss/kvalobs/trunk@3702 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/lib/kvalobs/kvDataOperations.cc
+++ src/lib/kvalobs/kvDataOperations.cc
@@ -186,7 +186,14 @@ void hqc_interpol( kvData & d, float new_val )
 void hqc_distribute( kvData & d, float new_val )
 {
   ::kvalobs::correct( d, new_val );
-  setFhqc_( d, 6 );
+
+  const int old_fd = c.controlinfo().flag( fd );
+  const int new_fd = (old_fd == 4 || old_fd == 8 || old_fd == 0xA) ? 0xA : 9;
+
+  kvControlInfo ci = d.controlinfo();
+  ci.set( fd, new_fd );
+  ci.set( fhqc, 6 ); // manual redistribution
+  d.controlinfo( ci );
 }
 
 void hqc_auto_correct( kvData & d, float new_val )
