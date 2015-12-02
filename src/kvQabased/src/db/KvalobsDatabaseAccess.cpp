01c57e835e6ad71f75685ea34a6a1d4a8c54a216
Michael Akinde <michaeloa@met.no>
2015-12-02 09:12:42 +0100

    Reduced TXT2 to advice, as Eclipse seems to struggle with that formatting.
    Summary: Lowered the TXT2 lint check (tabs for spaces) in order
    
    Test Plan:
    Find a file with the Tab issue. Run arc lint on that file; verify that it checks out as OK.
    (Note: lint will still inform about the problem).
    
    Reviewers: #kvalobs, vegardb
    
    Reviewed By: #kvalobs, vegardb
    
    Differential Revision: https://phab.met.no/D1214

--- src/kvQabased/src/db/KvalobsDatabaseAccess.cpp
+++ src/kvQabased/src/db/KvalobsDatabaseAccess.cpp
@@ -1,5 +1,5 @@
 /*
- Kvalobs - Free Quality Control Software for Meteorological Observations 
+ Kvalobs - Free Quality Control Software for Meteorological Observations
 
  Copyright (C) 2010 met.no
 
@@ -13,17 +13,17 @@
  This file is part of KVALOBS
 
  KVALOBS is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License as 
- published by the Free Software Foundation; either version 2 
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.
- 
+
  KVALOBS is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  General Public License for more details.
- 
- You should have received a copy of the GNU General Public License along 
- with KVALOBS; if not, write to the Free Software Foundation Inc., 
+
+ You should have received a copy of the GNU General Public License along
+ with KVALOBS; if not, write to the Free Software Foundation Inc.,
  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
