3b7951d96405eddf680aa8e1015148ba59b78e67
Alexander Bürger <alexanderb@met.no>
2015-12-09 09:00:09 +0100

    kvsubscribe test only if HAVE_GMOCK

--- src/lib/kvsubscribe/test/kvsubscribe.mk
+++ src/lib/kvsubscribe/test/kvsubscribe.mk
@@ -1,9 +1,13 @@
+if HAVE_GMOCK
+
 check_PROGRAMS = kvsubscribetest
 TESTS = $(check_PROGRAMS)
 
 kvsubscribetest_CPPFLAGS = $(AM_CPPFLAGS) $(gmock_CFLAGS) $(gtest_CFLAGS)
 kvsubscribetest_LDFLAGS = -pthread
 kvsubscribetest_LDADD = libkvalobs_kvsubscribe.la $(BOOST_DATE_TIME_LIB) $(gmock_LIBS) $(gtest_LIBS)
+
+endif # HAVE_GMOCK
 
 kvsubscribetest_SOURCES = \
     test/test.cpp \
