9223c26dc52ce493aa5b96f9d5de8a7bf9926ba6
Børge Moe <borge.moe@met.no>
2025-04-02 13:25:40 +0200

    Add option to print a today date after the version version-YYYYMMDD

--- version.sh
+++ version.sh
@@ -1,3 +1,35 @@
 #! /bin/bash
 
-grep 'AC_INIT(' configure.ac | sed 's/AC_INIT( *\[kvalobs\] *, *\[\(.*\)\] *,.*/\1/'
+usage="\
+Usage: version [-h] [-d]
+
+This script get the version of kvalobs from configute.ac.
+
+If the -d option is given it will print the version and the date on
+the form version-YYYYMMDD.
+
+-h display this help and exit.
+-d print version and date.
+"
+
+version=$(grep 'AC_INIT(' configure.ac | sed 's/AC_INIT( *\[kvalobs\] *, *\[\(.*\)\] *,.*/\1/')
+today=""
+
+while getopts "hd" opt; do
+  case $opt in
+    h)
+      echo "$usage"
+      exit 0
+      ;;
+    d)
+      today="-$(date +'%Y%m%d')"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      echo "$usage" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "$version$today"
